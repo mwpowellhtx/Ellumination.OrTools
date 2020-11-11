bd2477af694105a9221eeb76c674354455e8e626
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-11 18:16:26 -0500

    Refactored projects and name spaces.

--- src/tools/Common/CommonAssemblyInfo.cs
+++ src/tools/Common/CommonAssemblyInfo.cs
@@ -3,8 +3,8 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyProduct("Kingdom.OrTools")]
-[assembly: AssemblyCompany("Kingdom Software")]
-[assembly: AssemblyCopyright("Copyright (c) 2015-2019")]
+[assembly: AssemblyProduct("Ellumination.OrTools")]
+[assembly: AssemblyCompany("Ellumination Software")]
+[assembly: AssemblyCopyright("Copyright (c) 2015-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
