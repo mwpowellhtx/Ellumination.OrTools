eb743dd9d7a3855f5e85b19918d84357c4d21753
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-12 14:16:29 -0500

    Added Context bits.

---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Context.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    using Google.OrTools.ConstraintSolver;
+
+    // TODO: TBD: then from here, context invokes model ability to search...
+    // TODO: TBD: with or without search params...
+    // TODO: TBD: and I should think also delegation of the response...
+    /// <summary>
+    /// Represents the basic building blocks of a <see cref="RoutingModel"/> Context. While
+    /// changes to the moving parts, Nodes or Vehicles, as the case may be, depending on the
+    /// domain model, is possible, we advise against it in a given Context. If such mutations
+    /// are desired, then create a new Context instance with those moving parts having been
+    /// mutated.
+    /// </summary>
+    public abstract class Context : IDisposable
+    {
+        /// <summary>
+        /// Gets the NodeCount.
+        /// </summary>
+        public int NodeCount { get; }
+
+        /// <summary>
+        /// Gets the VehicleCount.
+        /// </summary>
+        public int VehicleCount { get; }
+
+        /// <summary>
+        /// Gets the Depot used to inform the Context.
+        /// </summary>
+        public int? Depot { get; }
+
+        /// <summary>
+        /// Gets the Vehicle Start and End Endpoints. Each of the Endpoint indices
+        /// must align with the <see cref="NodeCount"/>.
+        /// </summary>
+        public virtual IEnumerable<(int start, int end)> Endpoints { get; protected set; }
+
+        private RoutingModel _model;
+
+        /// <summary>
+        /// Gets the Manager associated with the Context.
+        /// </summary>
+        public RoutingIndexManager Manager { get; protected set; }
+
+        /// <summary>
+        /// Gets the Model associated with the Context.
+        /// </summary>
+        public virtual RoutingModel Model => this._model ?? (this._model = ModelParameters == null
+            ? new RoutingModel(this.Manager)
+            : new RoutingModel(this.Manager, this.ModelParameters)
+        );
+
+        /// <summary>
+        /// Gets the Solver associated with the <see cref="Model"/>.
+        /// </summary>
+        protected virtual Solver Solver => this.Model?.solver();
+
+        /// <summary>
+        /// Gets or Sets the Parameters associated with the Context. May be Null, in which
+        /// case nothing is relayed to the <see cref="Model"/>, or Set up until the moment
+        /// when <see cref="Model"/> is warmed up, at which point the opportunity will have
+        /// been lost to have set or configured the Parameters accordingly.
+        /// </summary>
+        /// <see cref="Model"/>
+        private RoutingModelParameters ModelParameters { get; }
+
+        /// <summary>
+        /// Initializes the Context.
+        /// </summary>
+        /// <returns></returns>
+        protected virtual bool TryOnInitialize()
+        {
+            bool TryInitializeDepot(int nodeCount, int vehicleCount, int? depot)
+            {
+                var tried = depot != null;
+                var actual = depot ?? default;
+
+                if (tried)
+                {
+                    this.Manager = new RoutingIndexManager(nodeCount, vehicleCount, actual);
+                    this.Endpoints = Enumerable.Range(0, vehicleCount).Select(_ => (actual, actual)).ToArray();
+                }
+
+                return tried;
+            }
+
+            bool TryInitializeEndpoints(int nodeCount, int vehicleCount, IEnumerable<(int start, int end)> endpoints)
+            {
+                var tried = endpoints != null;
+                var actual = endpoints.OrEmpty();
+
+                if (tried)
+                {
+                    var starts = actual.Select(x => x.start).ToArray();
+                    var ends = actual.Select(x => x.end).ToArray();
+                    this.Manager = new RoutingIndexManager(nodeCount, vehicleCount, starts, ends);
+                }
+
+                return tried;
+            }
+
+            return TryInitializeDepot(this.NodeCount, this.VehicleCount, this.Depot)
+                || TryInitializeEndpoints(this.NodeCount, this.VehicleCount, this.Endpoints);
+        }
+
+        protected Context(RoutingModelParameters modelParameters = null)
+            : this(default, null)
+        {
+        }
+
+        protected Context(int depot, RoutingModelParameters modelParameters = default)
+        {
+        }
+
+        /// <summary>
+        /// Default Protected Constructor.
+        /// </summary>
+        /// <param name="vehicleCount"></param>
+        /// <param name="nodeCount"></param>
+        /// <param name="depot"></param>
+        protected Context(int nodeCount, int vehicleCount, int depot = default)
+            : this(nodeCount, vehicleCount, depot, null)
+        {
+        }
+
+        /// <summary>
+        /// Default Protected Constructor.
+        /// </summary>
+        /// <param name="vehicleCount"></param>
+        /// <param name="nodeCount"></param>
+        /// <param name="depot"></param>
+        /// <param name="modelParameters"></param>
+        protected Context(int nodeCount, int vehicleCount, int depot = default, RoutingModelParameters modelParameters = default)
+        {
+            this.NodeCount = nodeCount;
+            this.VehicleCount = vehicleCount;
+            // TODO: TBD: verify that depot indexes are correct...
+            this.Depot = depot;
+            this.ModelParameters = modelParameters;
+            this.TryOnInitialize();
+        }
+
+        /// <summary>
+        /// Default Protected Constructor.
+        /// </summary>
+        /// <param name="vehicleCount"></param>
+        /// <param name="nodeCount"></param>
+        /// <param name="starts"></param>
+        /// <param name="ends"></param>
+        protected Context(int nodeCount, int vehicleCount, IEnumerable<int> starts, IEnumerable<int> ends)
+            : this(nodeCount, vehicleCount, starts, ends, null)
+        {
+        }
+
+        /// <summary>
+        /// Default Protected Constructor.
+        /// </summary>
+        /// <param name="vehicleCount"></param>
+        /// <param name="nodeCount"></param>
+        /// <param name="starts"></param>
+        /// <param name="ends"></param>
+        /// <param name="modelParameters"></param>
+        protected Context(int nodeCount, int vehicleCount, IEnumerable<int> starts, IEnumerable<int> ends, RoutingModelParameters modelParameters)
+        {
+            this.NodeCount = nodeCount;
+            this.VehicleCount = vehicleCount;
+            // TODO: TBD: verify that depot indexes are correct...
+            this.Endpoints = starts.Zip(ends, (start, end) => (start, end)).ToArray();
+            this.ModelParameters = modelParameters;
+            this.TryOnInitialize();
+        }
+
+        /// <summary>
+        /// Gets whether the object IsDisposed.
+        /// </summary>
+        protected bool IsDisposed { get; private set; }
+
+        /// <summary>
+        /// Disposes of the object.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !this.IsDisposed)
+            {
+                // TODO: TBD: determine whether Model **AND** Manager both need to be disposed...
+                this.Manager?.Dispose();
+                this.Model?.Dispose();
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            this.IsDisposed = true;
+        }
+    }
+}
---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Context{TNode, TVehicle}.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    using Google.OrTools.ConstraintSolver;
+
+    // https://developers.google.com/optimization/routing/pickup_delivery
+    // https://developers.google.com/optimization/reference/constraint_solver/routing_index_manager/RoutingIndexManager/
+    // https://github.com/google/or-tools/blob/v8.0/ortools/constraint_solver/routing_index_manager.cc#L26
+    // https://github.com/google/or-tools
+    // https://developers.google.com/optimization/routing/routing_options
+    // https://developers.google.com/optimization/routing/routing_tasks
+    // https://developers.google.com/optimization/routing/routing_tasks#setting-start-and-end-locations-for-routes
+    // https://developers.google.com/optimization/routing/penalties
+    // https://developers.google.com/optimization/routing/google_direction
+    // https://developers.google.com/optimization/routing/cvrptw_resources
+    // https://developers.google.com/optimization/routing/vrptw
+    // https://developers.google.com/optimization/routing/pickup_delivery
+    // https://developers.google.com/optimization/routing/cvrp#c_1
+    // https://developers.google.com/optimization/routing/tsp
+    // https://developers.google.com/optimization/routing
+    // https://github.com/settings/repositories
+    // https://github.com/mwpowellhtx/Ellumination.OrTools
+
+    /// <summary>
+    /// See also <see cref="Context"/> for notes. Same concerns vetted with this Context as with
+    /// the base class. Basically, if changes to the moving parts, either <see cref="Nodes"/>
+    /// or <see cref="Vehicles"/> or both, then we suggest creating a fresh <see cref="Context"/>
+    /// instance.
+    /// </summary>
+    /// <typeparam name="TNode">From a domain modeling perspective &quot;Node&quot; is simply
+    /// a moniker. Used to represent the destination or points of interest being traversed by
+    /// each <typeparamref name="TVehicle"/>.</typeparam>
+    /// <typeparam name="TVehicle">From a domain modeling perspective, &quot;Vehicle&quot;
+    /// is just a moniker. Used to represent whatever or whom ever is traversing the network
+    /// of <typeparamref name="TNode"/> instances during the Routing solution.</typeparam>
+    public abstract class Context<TNode, TVehicle> : Context
+    {
+        /// <summary>
+        /// Gets or Sets the Nodes.
+        /// </summary>
+        /// <remarks>At this level Nodes and Vehicles are simply along for the ride. Any solver
+        /// initialization that will have been performed will have been done at the base class
+        /// level.</remarks>
+        public virtual IEnumerable<TNode> Nodes { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Vehicles.
+        /// </summary>
+        public virtual IEnumerable<TVehicle> Vehicles { get; set; }
+
+        /// <summary>
+        /// Protected Context Constructor with unspecied <see cref="RoutingModelParameters"/>.
+        /// </summary>
+        /// <param name="nodes">The number of nodes or locations involved in the Context.</param>
+        /// <param name="vehicles">The number of vehicles involved in the Context.</param>
+        /// <param name="depot">Meaning &quot;home base&quot; or &quot;headquarters&quot;,
+        /// basically where ever the vehicles are operating as their base of operations for
+        /// scheduling purposes.</param>
+        protected Context(IEnumerable<TNode> nodes, IEnumerable<TVehicle> vehicles, int depot = 0)
+            : this(nodes, vehicles, depot, null)
+        {
+        }
+
+        /// <summary>
+        /// Protected Context Constructor.
+        /// </summary>
+        /// <param name="nodes">The number of nodes or locations involved in the Context.</param>
+        /// <param name="vehicles">The number of vehicles involved in the Context.</param>
+        /// <param name="depot">Meaning &quot;home base&quot; or &quot;headquarters&quot;,
+        /// basically where ever the vehicles are operating as their base of operations for
+        /// scheduling purposes.</param>
+        /// <param name="parameters"></param>
+        protected Context(IEnumerable<TNode> nodes, IEnumerable<TVehicle> vehicles, int depot = 0, RoutingModelParameters parameters = null)
+            : base(nodes.OrEmpty().Count(), vehicles.OrEmpty().Count(), depot, parameters)
+        {
+            // TODO: TBD: verify that the depot index is valid, that is, within range of the nodes.
+            this.Nodes = nodes.OrEmpty().ToArray();
+            this.Vehicles = vehicles.OrEmpty().ToArray();
+        }
+
+        /// <summary>
+        /// Protected Context Constructor with unspecied <see cref="RoutingModelParameters"/>.
+        /// </summary>
+        /// <param name="nodes">The number of nodes or locations involved in the Context.</param>
+        /// <param name="vehicles">The number of vehicles involved in the Context.</param>
+        /// <param name="starts">Not every Vehicle necessarily Starts at the same location.</param>
+        /// <param name="ends">Additionally, not every Vehicle may End at the same location.</param>
+        protected Context(IEnumerable<TNode> nodes, IEnumerable<TVehicle> vehicles, IEnumerable<int> starts, IEnumerable<int> ends)
+            : this(nodes, vehicles, starts, ends, null)
+        {
+        }
+
+        /// <summary>
+        /// Protected Context Constructor.
+        /// </summary>
+        /// <param name="nodes">The number of nodes or locations involved in the Context.</param>
+        /// <param name="vehicles">The number of vehicles involved in the Context.</param>
+        /// <param name="starts">Not every Vehicle necessarily Starts at the same location.</param>
+        /// <param name="ends">Additionally, not every Vehicle may End at the same location.</param>
+        /// <param name="modelParameters"></param>
+        protected Context(IEnumerable<TNode> nodes, IEnumerable<TVehicle> vehicles, IEnumerable<int> starts, IEnumerable<int> ends, RoutingModelParameters modelParameters)
+            : base(nodes.OrEmpty().Count(), vehicles.OrEmpty().Count(), starts, ends, modelParameters)
+        {
+            this.Nodes = nodes.OrEmpty().ToArray();
+            this.Vehicles = vehicles.OrEmpty().ToArray();
+        }
+    }
+}
