04865b0ed3d1d5432fa00940857c5ea11801e4f8
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-12 14:16:38 -0500

    Added Dimension placeholder bits.

---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Dimension{TContext, TNode, TVehicle}.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    // TODO: TBD: ditto basically a placeholder at this moment...
+    public abstract class Dimension<TNode, TVehicle, TContext> : Dimension<TContext>
+        where TContext : Context<TNode, TVehicle>
+    {
+        /// <summary>
+        /// Protected Constructor.
+        /// </summary>
+        /// <param name="context"></param>
+        protected Dimension(TContext context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Dimension{TContext}.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    // TODO: TBD: will consider dimension bits next...
+    // TODO: TBD: for now this is simply a placeholder...
+    // TODO: TBD: with the intention of cross referencing with the Context in some way shape or form...
+    public abstract class Dimension<TContext>
+        where TContext : Context
+    {
+        /// <summary>
+        /// Gets the Context associated with the Dimension.
+        /// </summary>
+        protected TContext Context { get; }
+
+        /// <summary>
+        /// Protected Constructor.
+        /// </summary>
+        /// <param name="context"></param>
+        protected Dimension(TContext context)
+        {
+            this.Context = context;
+        }
+    }
+}
