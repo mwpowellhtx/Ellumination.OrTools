ba8f747fd920878079427765de5b12a555b280b4
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-13 16:11:32 -0500

    Added reflection extension methods.

---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Extensions/ReflectionExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Reflection
+{
+    /// <summary>
+    /// Establishes a handful of useful Reflection extension methods.
+    /// </summary>
+    internal static class ReflectionExtensionMethods
+    {
+        /// <summary>
+        /// Returns whether <paramref name="root"/> <see cref="Type.IsAssignableFrom"/>
+        /// <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static bool IsAssignableFrom<T>(this Type root) => root.IsAssignableFrom(typeof(T));
+    }
+}
