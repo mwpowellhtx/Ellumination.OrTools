1f4a70959d92b2a49f78a3fae770a842efbcd65e
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-24 17:53:20 -0500

    Ditto clarification between Default Context usage and Domain usage.

---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Events/DefaultRoutingAssignmentEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    /// <summary>
+    /// Provides a set of <see cref="EventArgs"/> for use during the Routing Assignment process.
+    /// </summary>
+    /// <see cref="Routing.Context"/>
+    public class DefaultRoutingAssignmentEventArgs : RoutingAssignmentEventArgs<Context>
+    {
+        /// <summary>
+        /// Internal constructor.
+        /// </summary>
+        /// <param name="context">A <see cref="Context"/> instance.</param>
+        /// <param name="vehicleIndex">A Vehicle index.</param>
+        /// <param name="nodeIndex">A Node index.</param>
+        internal DefaultRoutingAssignmentEventArgs(Context context, int vehicleIndex, int nodeIndex)
+            : base(context, vehicleIndex, nodeIndex)
+        {
+        }
+    }
+}
---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Events/RoutingAssignmentEventArgs{TContext}.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    /// <summary>
+    /// Provides a set of <see cref="EventArgs"/> for use during the Routing Assignment process.
+    /// </summary>
+    /// <typeparam name="TContext"></typeparam>
+    /// <see cref="Routing.Context"/>
+    public class RoutingAssignmentEventArgs<TContext> : EventArgs
+        where TContext : Context
+    {
+        /// <summary>
+        /// Gets the Context.
+        /// </summary>
+        public TContext Context { get; }
+
+        /// <summary>
+        /// Gets the VehicleIndex.
+        /// </summary>
+        public int VehicleIndex { get; }
+
+        /// <summary>
+        /// Gets the NodeIndex.
+        /// </summary>
+        public int NodeIndex { get; }
+
+        /// <summary>
+        /// Internal constructor.
+        /// </summary>
+        /// <param name="context">A <typeparamref name="TContext"/> instance.</param>
+        /// <param name="vehicleIndex">A Vehicle index.</param>
+        /// <param name="nodeIndex">A Node index.</param>
+        internal RoutingAssignmentEventArgs(TContext context, int vehicleIndex, int nodeIndex)
+        {
+            this.Context = context;
+            this.VehicleIndex = vehicleIndex;
+            this.NodeIndex = nodeIndex;
+        }
+    }
+}
