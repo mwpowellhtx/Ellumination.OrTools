e305f53d6d477ceda7aaae83a2c9d7de5722f7f7
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-30 10:52:03 -0500

    Resolved merge conflicts.

---
+++ src/Test.Ellumination.OrTools.Fibonacci/FibonacciCalculatorTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+
+    public class FibonacciCalculatorTests : TestFixtureBase
+    {
+        /// <summary>
+        /// Constructs the fixture given parameters.
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        public FibonacciCalculatorTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Gets the ExpectedSequences.
+        /// </summary>
+        private IDictionary<int, IEnumerable<int>> ExpectedSequences { get; set; }
+
+        [Background]
+        public void CalculatorBackground(int count)
+        {
+            $"Initialize the {nameof(ExpectedSequences)}".x(() =>
+            {
+                // 5, 10, 15 elements...
+                var sequence = Range(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377);
+
+                this.ExpectedSequences = new Dictionary<int, IEnumerable<int>>();
+
+                Range(5, 10, 15).ToList().ForEach(key => this.ExpectedSequences[key] = sequence.Take(key).ToArray());
+            });
+
+            $"Fibonacci {nameof(count)} is 5".x(() => count = 5);
+
+            $"Initialize this.{nameof(Calculator)}".x(() => this.Calculator[count - 1].AssertEqual(
+                this.ExpectedSequences[count].Last()
+            ));
+
+            $"Verify this.{nameof(Calculator)}".x(() => this.Calculator.AssertEqual(
+                this.ExpectedSequences[count]
+            ));
+        }
+
+        [Scenario]
+        public void Sequence_Does_Not_Recalculate(int count, int middle)
+        {
+            $"Fibonacci {nameof(count)} is 5".x(() => count = 5);
+
+            $"Fibonacci {nameof(middle)} is 3".x(() => middle = 3);
+
+            $"Verify element at {middle - 1}".x(() => this.Calculator[middle - 1].AssertEqual(
+                this.ExpectedSequences[count].ElementAt(middle - 1)
+            ));
+
+            $"Verify this.{nameof(Calculator)}".x(() => this.Calculator.AssertEqual(
+                this.ExpectedSequences[count]
+            ));
+        }
+
+        [Scenario]
+        public void Sequence_Calculates(int count)
+        {
+            $"Fibonacci {nameof(count)} is 10".x(() => count = 10);
+
+            $"Request the element at {count - 1}".x(() => this.Calculator[count - 1].AssertEqual(
+                this.ExpectedSequences[count].Last()
+            ));
+
+            $"And verify the sequence itself".x(() => this.Calculator.AssertEqual(
+                this.ExpectedSequences[count]
+            ));
+        }
+
+        [Scenario]
+        public void Sequence_Can_Expand(int count)
+        {
+            $"Fibonacci {nameof(count)} is 15".x(() => count = 15);
+
+            $"Starting sequence count was not {count}".x(() => this.Calculator.AssertTrue(x => x.Count() < count));
+
+            $"Expand to {count} and verify".x(() => this.Calculator.Expand(count).AssertEqual(
+                this.ExpectedSequences[count]
+            ));
+        }
+    }
+}
---
+++ src/Test.Ellumination.OrTools.Fibonacci/TestFixtureBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+
+    public abstract class TestFixtureBase : IDisposable
+    {
+        /// <summary>
+        /// Returns the Range of <paramref name="values"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        protected static IEnumerable<T> Range<T>(params T[] values)
+        {
+            foreach (var value in values)
+            {
+                yield return value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the OutputHelper.
+        /// </summary>
+        protected ITestOutputHelper OutputHelper { get; }
+
+        /// <summary>
+        /// Gets the Calculator for use throughout the fixture.
+        /// </summary>
+        protected virtual FibonacciCalculator Calculator { get; private set; }
+
+        /// <summary>
+        /// Constructs the fixture given parameters.
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        protected TestFixtureBase(ITestOutputHelper outputHelper)
+        {
+            this.OutputHelper = outputHelper;
+        }
+
+        /// <summary>
+        /// Performs some Base Background initialization.
+        /// </summary>
+        [Background]
+        public void BaseBackground()
+        {
+            $"Initializes the {nameof(Calculator)}".x(() => this.Calculator = new FibonacciCalculator());
+
+            $"Verify the {nameof(Calculator)}".x(() => Calculator.AssertNotNull().AssertEqual(Range(0, 1)));
+        }
+
+        /// <summary>
+        /// Gets whether IsDisposed.
+        /// </summary>
+        protected bool IsDisposed { get; private set; }
+
+        /// <summary>
+        /// Disposes the object.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            this.IsDisposed = true;
+        }
+    }
+}
