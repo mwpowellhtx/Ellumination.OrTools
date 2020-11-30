1e1a743b82d14c0ef8053312488f79e4c2b35aa9
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-30 10:34:44 -0500

    Resolved merge conflicts.

---
+++ code/Ellumination.OrTools.Sat.CodeGeneration.Attributes/Parameters/SupportProtocolBufferParametersAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ellumination.OrTools.Sat.CodeGeneration.Attributes
+{
+    using Code.Generation.Roslyn;
+
+    /// <summary>
+    /// The Code Generator must be named CpSatParametersGenerator in the
+    /// Ellumination.OrTools.Sat.CodeGeneration.Generators namespace furnished from a separate
+    /// assembly.
+    /// </summary>
+    /// <inheritdoc />
+    [AttributeUsage(AttributeTargets.Assembly)
+     , CodeGenerationAttribute("Ellumination.OrTools.Sat.CodeGeneration.Generators.CpSatParametersAssemblyCodeGenerator, Ellumination.OrTools.Sat.CodeGeneration")
+     , Conditional("CodeGeneration")]
+    public class SupportProtocolBufferParametersAttribute : Attribute
+    {
+    }
+}
