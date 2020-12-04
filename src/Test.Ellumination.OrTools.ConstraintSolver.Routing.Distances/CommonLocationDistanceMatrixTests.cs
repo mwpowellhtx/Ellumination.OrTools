8c177d943ba21f85b77fd3cbf12e98ee9f43fe78
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-04 09:48:15 -0500

    Added LocationDistanceMatrix unit tests.

---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing.Distances/CommonLocationDistanceMatrixTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.Distances
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+
+    public class CommonLocationDistanceMatrixTests : LocationDistanceMatrixTests<LocationDistanceMatrix>
+    {
+        public CommonLocationDistanceMatrixTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Gets or Sets the OtherLocations.
+        /// </summary>
+        private IEnumerable<string> OtherLocations { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the OtherInstance.
+        /// </summary>
+        private LocationDistanceMatrix OtherInstance { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="locations"></param>
+        [Background]
+        public void CommonLocationBackground(LocationDistanceMatrix instance, IEnumerable<string> locations)
+        {
+            $"Verify default this.{nameof(this.Instance)}".x(() => this.Instance.AssertNotNull()
+                .AssertEqual(this.ExpectedLocations, x => x.Locations)
+            );
+
+            $"{nameof(this.OnInitializeInstance)} new {nameof(instance)} with {nameof(locations)}".x(
+                () => this.OnInitializeInstance((x, a) => (this.Instance, this.ExpectedLocations) = (x, a)
+                    , out instance, out locations)
+            );
+
+            $"{nameof(this.OnInitializeInstance)} new {nameof(instance)} with {nameof(locations)}".x(
+                () => this.OnInitializeInstance((x, a) => (this.OtherInstance, this.OtherLocations) = (x, a)
+                    , out instance, out locations)
+            );
+
+            $"Verify this.{nameof(this.ExpectedLocations)}".x(() => this.ExpectedLocations.AssertNotNull()
+                .AssertCollectionNotEmpty()
+            );
+
+            $"Verify this.{nameof(this.OtherLocations)}".x(() => this.OtherLocations.AssertNotNull()
+                .AssertCollectionNotEmpty()
+            );
+        }
+
+        /// <summary>
+        /// Verifies the Matrix Locations. We intentionally postpone the collection ordering,
+        /// which has the added benefit of further validating whether we have expected results
+        /// from the Matrix.
+        /// </summary>
+        /// <see cref="OtherInstance"/>
+        /// <see cref="OtherLocations"/>
+        /// <see cref="MatrixTests{M}.Instance"/>
+        /// <see cref="LocationDistanceMatrixTests{M}.ExpectedLocations"/>
+        [Scenario]
+        public void Verify_Matrix_Locations()
+        {
+            $"Verify this.{nameof(this.Instance)}".x(() => this.Instance.AssertNotNull()
+                .AssertEqual(this.ExpectedLocations.OrderBy(y => y), x => x.Locations)
+            );
+
+            $"Verify this.{nameof(this.OtherInstance)}".x(() => this.OtherInstance.AssertNotNull()
+                .AssertEqual(this.OtherLocations.OrderBy(y => y), x => x.Locations)
+            );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="merged"></param>
+        /// <param name="mergeLocations"></param>
+        [Scenario]
+        public void Can_Merge(LocationDistanceMatrix merged, IEnumerable<string> mergeLocations)
+        {
+            $"Merge {nameof(mergeLocations)}".x(
+                () => mergeLocations = this.ExpectedLocations.Concat(this.OtherLocations).ToArray()
+            );
+
+            $"Verify {nameof(merged)}".x(
+                () => merged = this.Instance.Merge(this.OtherInstance).AssertNotNull()
+            );
+
+            $"Verify {nameof(merged)}.{nameof(merged.Locations)}".x(
+                () => merged.Locations.AssertEqual(mergeLocations.OrderBy(x => x))
+            );
+        }
+    }
+}
---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing.Distances/LocationDistanceMatrixTests{M}.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.Distances
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+    using static Matrix;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="M"></typeparam>
+    public abstract class LocationDistanceMatrixTests<M> : DistanceMatrixTests<M>
+        where M : LocationDistanceMatrix, new()
+    {
+        protected LocationDistanceMatrixTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Gets a Name for use within the unit test.
+        /// </summary>
+        protected static string Name => $"{Guid.NewGuid()}".ToLower();
+
+        /// <summary>
+        /// Gets or Sets the ExpectedLocations.
+        /// </summary>
+        protected virtual IEnumerable<string> ExpectedLocations { get; set; } = Range<string>();
+
+        /// <summary>
+        /// Starts out being of Zero Length.
+        /// </summary>
+        protected override int ExpectedLength { get; } = default;
+
+        /// <inheritdoc/>
+        protected override void OnCreateInstance(out M instance) =>
+            this.OnCreateInstance(out instance, Range<string>().ToArray());
+
+        /// <summary>
+        /// Creates an <paramref name="instance"/> given <paramref name="locations"/>.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="locations"></param>
+        protected virtual void OnCreateInstance(out M instance, params string[] locations) =>
+            instance = Activator.CreateInstance(typeof(M), (IEnumerable<string>)locations)
+                .AssertIsType<M>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="onSetLocations"></param>
+        /// <param name="instance"></param>
+        /// <param name="locations"></param>
+        protected virtual void OnInitializeInstance(
+            Action<LocationDistanceMatrix, IEnumerable<string>> onSetLocations
+            , out M instance, out IEnumerable<string> locations)
+        {
+            locations = Range(Name, Name, Name).AssertEqual(3, x => x.Count());
+            this.OnCreateInstance(out instance, locations.ToArray());
+            onSetLocations.Invoke(instance.AssertNotNull(), locations);
+        }
+
+        /// <summary>
+        /// Verifies the <paramref name="matrix"/> given <paramref name="index"/> and
+        /// <paramref name="expected"/> value.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="index"></param>
+        /// <param name="expected"></param>
+        protected virtual void VerifyMatrixValue(M matrix, (string x, string y) index, int? expected = null) =>
+            matrix[index.x, index.y].AssertEqual(index.x == index.y ? default(int) : expected);
+
+        /// <inheritdoc/>
+        protected override void OnVerifyMatrixBeforeBackground(out M instance, out IEnumerable<(int x, int y)> instanceIndices)
+        {
+            base.OnVerifyMatrixBeforeBackground(out instance, out instanceIndices);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnVerifyMatrixAfterBackground(out M instance, out IEnumerable<(int x, int y)> instanceIndices)
+        {
+            base.OnVerifyMatrixAfterBackground(out instance, out instanceIndices);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="instanceIndices"></param>
+        /// <param name="locations"></param>
+        [Background]
+        public void LocationDistanceMatrixBackground(M instance, IEnumerable<(int x, int y)> instanceIndices, IEnumerable<string> locations)
+        {
+            $"Zero {nameof(locations)}".x(() => locations = Range<string>());
+
+            $"Zero {nameof(this.ExpectedLocations)}".x(() => this.ExpectedLocations.AssertEqual(locations));
+
+            $"Obtain {nameof(instance)}".x(() => (instance = this.Instance).AssertNotNull());
+
+            $"Verify {nameof(instance)}.{nameof(instance.Length)}".x(() => instance.Length.AssertEqual(default));
+
+            $"Verify {nameof(instance)}.{nameof(instance.Locations)}".x(() => instance.Locations.AssertEqual(locations));
+        }
+    }
+}
