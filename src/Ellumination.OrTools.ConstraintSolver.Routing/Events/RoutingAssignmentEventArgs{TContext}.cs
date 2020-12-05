aac5b8fbde788746dd3919dc0beeedaf7607c58e
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-05 18:11:52 -0500

    Added previous node.

--- src/Ellumination.OrTools.ConstraintSolver.Routing/Events/RoutingAssignmentEventArgs{TContext}.cs
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Events/RoutingAssignmentEventArgs{TContext}.cs
@@ -15,15 +15,22 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing
         /// </summary>
         public TContext Context { get; }
 
+        // TODO: TBD: similarly with VehicleIndex? or Vehicle?
         /// <summary>
         /// Gets the VehicleIndex.
         /// </summary>
         public int VehicleIndex { get; }
 
+        // TODO: TBD: gets the NodeIndex? or the Node? in RoutingModel terms...
         /// <summary>
         /// Gets the NodeIndex.
         /// </summary>
         public int NodeIndex { get; }
+
+        /// <summary>
+        /// Gets the PreviousNodeIndex from which Routing occurred.
+        /// </summary>
+        public int? PreviousNodeIndex { get; }
 
         /// <summary>
         /// Internal constructor.
@@ -31,11 +38,13 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing
         /// <param name="context">A <typeparamref name="TContext"/> instance.</param>
         /// <param name="vehicleIndex">A Vehicle index.</param>
         /// <param name="nodeIndex">A Node index.</param>
-        internal RoutingAssignmentEventArgs(TContext context, int vehicleIndex, int nodeIndex)
+        /// <param name="previousNodeIndex">A Previous Node index from which Routing occurred.</param>
+        internal RoutingAssignmentEventArgs(TContext context, int vehicleIndex, int nodeIndex, int? previousNodeIndex)
         {
             this.Context = context;
             this.VehicleIndex = vehicleIndex;
             this.NodeIndex = nodeIndex;
+            this.PreviousNodeIndex = previousNodeIndex;
         }
     }
 }
