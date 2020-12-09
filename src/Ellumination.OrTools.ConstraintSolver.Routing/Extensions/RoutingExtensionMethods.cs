587261f2b0bef5c9b9cbfbdd3d6667ea611c6044
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-09 17:46:15 -0500

    Added coordinates extension methods.

--- src/Ellumination.OrTools.ConstraintSolver.Routing/Extensions/RoutingExtensionMethods.cs
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Extensions/RoutingExtensionMethods.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Ellumination.OrTools.ConstraintSolver.Routing
 {
@@ -16,5 +16,21 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing
             yield return ep.start;
             yield return ep.end;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xDim"></param>
+        /// <param name="yDim"></param>
+        /// <returns></returns>
+        public static IEnumerable<(int x, int y)> AsCoordinates(this IEnumerable<int> xDim, IEnumerable<int> yDim) =>
+            xDim.SelectMany(x => yDim.Select(y => (x, y)));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dim"></param>
+        /// <returns></returns>
+        public static IEnumerable<(int x, int y)> AsCoordinates(this IEnumerable<int> dim) => dim.AsCoordinates(dim);
     }
 }
