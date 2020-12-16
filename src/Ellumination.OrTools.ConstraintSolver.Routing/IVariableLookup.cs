d5ec5e38d9d76c91ebdbdd38e8cedffd3e09e380
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-16 15:46:05 -0500

    Added variable lookup capability.

---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/IVariableLookup.cs
@@ -0,0 +1,26 @@
+﻿namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    using Google.OrTools.ConstraintSolver;
+
+    /// <summary>
+    /// Provides a Variable Lookup. Assumes that the Variable can be indexed
+    /// by a <see cref="long"/> Index.
+    /// </summary>
+    public interface IVariableLookup
+    {
+        /// <summary>
+        /// Returns the <see cref="IntVar"/> Variable corresponding to the
+        /// <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        IntVar Get(long index);
+
+        /// <summary>
+        /// Provides a Get only Variable <see cref="IntVar"/> indexer.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        IntVar this[long index] { get; }
+    }
+}
---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/VariableLookup{T}.cs
@@ -0,0 +1,50 @@
+﻿namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    using Google.OrTools.ConstraintSolver;
+
+    /// <summary>
+    /// Provides a Variable Lookup base class. Assumes that <see cref="Source"/> Variables
+    /// are indexed by <see cref="long"/> indexes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class VariableLookup<T> : IVariableLookup
+    {
+        /// <summary>
+        /// Gets the Source informing the VariableLookup.
+        /// </summary>
+        protected T Source { get; }
+
+        /// <summary>
+        /// Constructs the Context corresponding to the <paramref name="source"/>.
+        /// </summary>
+        /// <param name="source"></param>
+        protected VariableLookup(T source)
+        {
+            this.Source = source;
+        }
+
+        /// <summary>
+        /// Returns the <see cref="IntVar"/> corresponding to the <paramref name="source"/>
+        /// at the <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        protected abstract IntVar Get(T source, long index);
+
+        /// <summary>
+        /// Returns the Variable corresponding to the <see cref="Source"/>
+        /// at the <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public virtual IntVar Get(long index) => this.Get(this.Source, index);
+
+        /// <summary>
+        /// Variable Get only indexer.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        /// <see cref="Get"/>
+        public virtual IntVar this[long index] => this.Get(index);
+    }
+}
