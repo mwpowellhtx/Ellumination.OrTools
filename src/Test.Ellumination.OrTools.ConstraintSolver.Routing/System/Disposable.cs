319a582be0b2f3bde2272969dcf30c21e3547612
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-19 17:32:25 -0500

    Updated Disposable support.

---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/System/Disposable.cs
@@ -0,0 +1,42 @@
+﻿namespace System
+{
+    /// <inheritdoc/>
+    public abstract class Disposable : IDisposableEx
+    {
+        /// <inheritdoc/>
+        public bool IsDisposed { get; private set; }
+
+        /// <inheritdoc/>
+        public event EventHandler<EventArgs> Disposing;
+
+        /// <summary>
+        /// <see cref="Disposing"/> Event dispatcher.
+        /// </summary>
+        /// <param name="e"></param>
+        protected void OnDisposing(EventArgs e) => this.Disposing?.Invoke(this, e);
+
+        /// <summary>
+        /// <see cref="Disposing"/> Event dispatcher.
+        /// </summary>
+        protected virtual void OnDisposing() => this.OnDisposing(EventArgs.Empty);
+
+        /// <summary>
+        /// Disposes the object.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !this.IsDisposed)
+            {
+                this.OnDisposing();
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            this.IsDisposed = true;
+        }
+    }
+}
---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/System/IDisposableEx.cs
@@ -0,0 +1,16 @@
+﻿namespace System
+{
+    /// <inheritdoc/>
+    public interface IDisposableEx : IDisposable
+    {
+        /// <summary>
+        /// Event occurs on Disposing.
+        /// </summary>
+        event EventHandler<EventArgs> Disposing;
+
+        /// <summary>
+        /// Gets whether the object IsDisposed.
+        /// </summary>
+        bool IsDisposed { get; }
+    }
+}
