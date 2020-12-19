f4f921beff97ec35d06aaf52eec2c8306d68e892
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-19 17:33:59 -0500

    Did a lot of refactoring into base and intermediate classes. Refactored scope solution verification and reported.

--- src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/Scopes/CaseStudyScope.cs
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/Scopes/CaseStudyScope.cs
@@ -6,115 +6,177 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
 {
     using Xunit;
     using Xunit.Abstractions;
+    using static String;
     using static TestFixtureBase;
 
     /// <summary>
     /// Specifies certain bits that are common throughout the case studies.
     /// </summary>
-    public abstract class CaseStudyScope : IDisposable
+    public abstract class CaseStudyScope : Disposable
     {
         /// <summary>
-        /// Gets or Sets the TotalDistance for use throughout the tests.
+        /// Gets the DimensionCoefficient, <c>100</c>.
         /// </summary>
-        internal int TotalDistance { get; set; } = default;
+        /// <value>100</value>
+        internal virtual int DimensionCoefficient { get; } = 100;
 
         /// <summary>
-        /// Gets the VehicleCount.
+        /// Gets the VehicleCap. Defaults to zero, <c>0</c>.
         /// </summary>
-        internal virtual int VehicleCount { get; } = default;
-
-        private IList<int?> _routeDistances;
+        /// <value>0</value>
+        internal virtual long VehicleCap { get; }
 
         /// <summary>
-        /// Gets the RouteDistances for the Case Study, indexed by Vehicle.
+        /// Gets the Depot, <c>default</c>.
+        /// Assumes Depot is the default, or zero.
         /// </summary>
-        internal virtual IList<int?> RouteDistances => (this._routeDistances ?? (
-            this._routeDistances = Range(0, this.VehicleCount).Select(_ => (int?)null).ToList()
-        )).AssertNotNull().AssertEqual(this.VehicleCount, _ => _.Count);
+        internal virtual int Depot { get; } = default;
 
         /// <summary>
-        /// Gets the <see cref="TotalDistance"/> Unit of Measure.
+        /// Gets the VehicleCount, <c>1</c>.
+        /// Assumes one single vehicle in the Case Study by default.
         /// </summary>
-        protected abstract string DistanceUnit { get; }
+        /// <value>1</value>
+        internal virtual int VehicleCount { get; } = 1;
 
         /// <summary>
-        /// Gets the OutputHelper.
+        /// Gets a Range of Indices corresponding to the <see cref="VehicleCount"/>.
         /// </summary>
-        protected ITestOutputHelper OutputHelper { get; }
-
-        protected CaseStudyScope(ITestOutputHelper outputHelper)
-        {
-            this.OutputHelper = outputHelper;
-        }
+        internal IEnumerable<int> VehicleIndices => Range(0, this.VehicleCount);
 
         /// <summary>
         /// Gets the <see cref="Matrix"/> Values associated with the Case Study.
         /// </summary>
         protected abstract int?[,] MatrixValues { get; }
 
+        private IList<long?> _routeDistances;
+        private IList<IList<int>> _solutionPaths;
+
         /// <summary>
-        /// 
+        /// Gets the RouteDistances for the Case Study, indexed by Vehicle.
+        /// </summary>
+        internal virtual IList<long?> RouteDistances => (this._routeDistances ?? (
+            this._routeDistances = this.VehicleIndices.Select(_ => (long?)null).ToList()
+        )).AssertNotNull().AssertEqual(this.VehicleCount, _ => _.Count);
+
+        /// <summary>
+        /// Gets a Default <see cref="SolutionPaths"/> Element.
         /// </summary>
         private static IList<int> DefaultSolutionPath => Range<int>().ToList();
-
-        private IList<IList<int>> _solutionPaths;
 
         /// <summary>
         /// Gets the SolutionPaths.
         /// </summary>
         internal IList<IList<int>> SolutionPaths => (this._solutionPaths ?? (
-            this._solutionPaths = Range(0, this.VehicleCount).Select(_ => DefaultSolutionPath).ToList()
+            this._solutionPaths = this.VehicleIndices.Select(_ => DefaultSolutionPath).ToList()
         )).AssertNotNull().AssertEqual(this.VehicleCount, _ => _.Count());
 
         /// <summary>
-        /// Event handler occurs On
-        /// <see cref="AssignableRoutingProblemSolver{TContext, TAssign}.Assign"/> event.
+        /// Gets the <see cref="ActualTotalDistance"/> Unit of Measure.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected virtual void OnProblemSolverAssign(object sender, DefaultRoutingAssignmentEventArgs e)
+        protected abstract string DistanceUnit { get; }
+
+        /// <summary>
+        /// Gets or Sets the ActualTotalDistance for use throughout the tests.
+        /// </summary>
+        internal long ActualTotalDistance { get; set; } = default;
+
+        /// <summary>
+        /// Gets or Sets the ExpectedTotalDistance.
+        /// </summary>
+        internal virtual long ExpectedTotalDistance { get; set; }
+
+        /// <summary>
+        /// Gets a Default <see cref="ExpectedPaths"/> Element.
+        /// </summary>
+        private static IEnumerable<int> DefaultExpectedPath => Range<int>();
+
+        private IList<int[]> _expectedPaths;
+
+        /// <summary>
+        /// Gets or Sets the ExpectedPaths.
+        /// </summary>
+        internal virtual IList<int[]> ExpectedPaths
         {
+            get => this._expectedPaths ?? (this._expectedPaths
+                = Range(0, this.VehicleCount).Select(_ => DefaultExpectedPath.ToArray()).ToList()
+            );
+            set => this._expectedPaths = value.OrEmpty().ToList();
         }
 
         /// <summary>
-        /// Override in order to Verify the Solution.
+        /// Gets the OutputHelper.
         /// </summary>
-        internal virtual void VerifySolution()
+        protected ITestOutputHelper OutputHelper { get; }
+
+        /// <summary>
+        /// Constructs the CaseStudyScope instance.
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        protected CaseStudyScope(ITestOutputHelper outputHelper)
         {
+            this.OutputHelper = outputHelper;
         }
 
         /// <summary>
-        /// Event occurs On <see cref="Dispose(bool)"/>.
+        /// Verifies and Reports the <see cref="SolutionPaths"/> and <see cref="ExpectedPaths"/>
+        /// and related bits.
         /// </summary>
-        internal event EventHandler<EventArgs> Disposing;
-
-        /// <summary>
-        /// Callback invokes the <see cref="Disposing"/> event.
-        /// </summary>
-        protected virtual void OnDisposing() => this.Disposing?.Invoke(this, EventArgs.Empty);
-
-        /// <summary>
-        /// Gets whether the object isDisposed.
-        /// </summary>
-        protected bool IsDisposed { get; private set; }
-
-        /// <summary>
-        /// Override in order to dispose the object.
-        /// </summary>
-        /// <param name="disposing"></param>
-        protected virtual void Dispose(bool disposing)
+        internal void VerifyAndReportSolution()
         {
-            if (disposing && !this.IsDisposed)
+            this.ActualTotalDistance.AssertEqual(this.ExpectedTotalDistance);
+
+            this.ExpectedPaths.AssertEqual(this.VehicleCount, x => x.Count);
+            this.SolutionPaths.AssertEqual(this.ExpectedPaths.Count, x => x.Count);
+
+            // Zipped for both Report and for Verification.
+            var zipped = this.SolutionPaths.Zip(this.ExpectedPaths, (a, e, i) => (a: a.ToArray(), e, i)).ToList();
+
+            void OnReportPaths((int[] a, int[] e, int i) tuple)
             {
-                this.OnDisposing();
-            }
-        }
+                var (a, e, i, aligned) = (
+                    tuple.a
+                    , tuple.e
+                    , tuple.i
+                    , tuple.a.SequenceEqual(tuple.e)
+                );
 
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            this.Dispose(true);
-            this.IsDisposed = true;
+                const char comma = ',';
+                var delim = $"\r\n    {comma} ";
+
+                var rendered = RenderTupleAssociates(delim
+                    , (nameof(a), Render(a))
+                    , (nameof(e), Render(e))
+                    , (nameof(i), Render(i))
+                    , (nameof(aligned), Render(aligned))
+                );
+
+                const string squareBrackets = "[]";
+
+                this.OutputHelper.WriteLine($"  {(i == 0 ? Empty : $"{comma} ")}"
+                    + $"{Join($"{i}", squareBrackets.ToArray())} {rendered}");
+            }
+
+            var scopeType = this.GetType();
+
+            this.OutputHelper.WriteLine($"{scopeType.FullName}.{nameof(this.SolutionPaths)}"
+                + $".{nameof(Enumerable.Zip)}({scopeType.FullName}.{nameof(this.ExpectedPaths)}"
+                + $", ...) = new[] {{");
+
+            zipped.ForEach(OnReportPaths);
+
+            this.OutputHelper.WriteLine($"}}");
+
+            void OnVerifyPaths((int[] a, int[] e, int i) tuple)
+            {
+                var (a, e, i) = tuple;
+                const int zero = default;
+                i.AssertTrue(_ => _ >= zero);
+                a.AssertEqual(e.Length, _ => _.Length);
+                a.AssertCollectionEqual(e);
+            }
+
+            zipped.ForEach(OnVerifyPaths);
         }
     }
 }
--- src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/Scopes/CaseStudyScope{TMatrix, TContext, TAssign, TProblemSolver}.cs
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/Scopes/CaseStudyScope{TMatrix, TContext, TAssign, TProblemSolver}.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 
 namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
@@ -10,29 +9,41 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
     using static TestFixtureBase;
 
     /// <summary>
-    /// Specifies certain bits that are common throughout the case studies.
+    /// Specifies certain bits that are common throughout the Case Studies.
     /// </summary>
+    /// <typeparam name="TMatrix"></typeparam>
+    /// <typeparam name="TContext"></typeparam>
+    /// <typeparam name="TAssign"></typeparam>
+    /// <typeparam name="TProblemSolver"></typeparam>
     public abstract class CaseStudyScope<TMatrix, TContext, TAssign, TProblemSolver> : CaseStudyScope
         where TMatrix : Distances.Matrix
         where TContext : RoutingContext
         where TAssign : RoutingAssignmentEventArgs<TContext>
         where TProblemSolver : AssignableRoutingProblemSolver<TContext, TAssign>
     {
-        protected CaseStudyScope(ITestOutputHelper outputHelper)
-            : base(outputHelper)
-        {
-        }
+        private TMatrix _matrix;
+        private TContext _context;
+        private TProblemSolver _problemSolver;
 
         /// <summary>
-        /// Gets the Matrix associated with the Case Study.
+        /// Returns a Created <typeparamref name="TMatrix"/> instance.
         /// </summary>
-        internal abstract TMatrix Matrix { get; }
+        /// <returns></returns>
+        /// <see cref="Matrix"/>
+        protected abstract TMatrix CreateMatrix();
 
         /// <summary>
-        /// Gets the Context associated with the Case Study.
+        /// Returns a Created <typeparamref name="TContext"/> instance.
         /// </summary>
-        internal abstract TContext Context { get; }
+        /// <returns></returns>
+        /// <see cref="Context"/>
+        protected abstract TContext CreateContext();
 
+        /// <summary>
+        /// Returns a new <typeparamref name="TProblemSolver"/> instance.
+        /// </summary>
+        /// <returns></returns>
+        /// <see cref="ProblemSolver"/>
         protected virtual TProblemSolver CreateProblemSolver()
         {
             var problemSolver = Activator.CreateInstance<TProblemSolver>();
@@ -40,15 +51,29 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
             return problemSolver;
         }
 
-        private TProblemSolver _problemSolver;
+        /// <summary>
+        /// Gets the Matrix associated with the Case Study.
+        /// </summary>
+        /// <see cref="CreateMatrix"/>
+        internal virtual TMatrix Matrix => this._matrix ?? (this._matrix = this.CreateMatrix());
 
-        // TODO: TBD: may be able to install this in the base class..
+        /// <summary>
+        /// Gets the Context associated with the Case Study.
+        /// </summary>
+        /// <see cref="CreateContext"/>
+        internal virtual TContext Context => this._context ?? (this._context = this.CreateContext());
+
         /// <summary>
         /// Gets or Sets the ProblemSolver.
         /// </summary>
-        internal TProblemSolver ProblemSolver => this._problemSolver ?? (
-            this._problemSolver = CreateProblemSolver()
-        );
+        /// <see cref="CreateProblemSolver"/>
+        internal TProblemSolver ProblemSolver => this._problemSolver ?? (this._problemSolver = CreateProblemSolver());
+
+        /// <inheritdoc/>
+        protected CaseStudyScope(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
 
         /// <summary>
         /// Event handler occurs On
@@ -58,31 +83,38 @@ namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
         /// <param name="e"></param>
         protected virtual void OnProblemSolverAssign(object sender, TAssign e)
         {
+            var (vehicle, node, previousNode) = (e.AssertNotNull().VehicleIndex, e.NodeIndex, e.PreviousNodeIndex);
+
+            // Should always have a SolutionPath corresponding to the Vehicle.
+            this.SolutionPaths.ElementAt(vehicle).AssertNotNull().Add(node);
+
+            if (previousNode.HasValue)
+            {
+                var transitCost = this.Context.GetArcCostForVehicle(previousNode ?? default, node, vehicle);
+
+                this.ActualTotalDistance += transitCost;
+
+                this.RouteDistances[vehicle] = (this.RouteDistances.ElementAt(vehicle) ?? default) + transitCost;
+            }
         }
 
-        /// <summary>
-        /// Override in order to dispose the object.
-        /// </summary>
-        /// <param name="disposing"></param>
-        protected override void Dispose(bool disposing)
+        /// <inheritdoc/>
+        protected override void OnDisposing()
         {
-            if (disposing && !this.IsDisposed)
+            void OnDisposeProblemSolver(TProblemSolver problemSolver)
             {
-                this.OnDisposing();
-
-                void OnDisposeProblemSolver(TProblemSolver problemSolver)
+                if (problemSolver != null)
                 {
-                    if (problemSolver != null)
-                    {
-                        problemSolver.Assign -= this.OnProblemSolverAssign;
-                        problemSolver.Dispose();
-                    }
+                    problemSolver.Assign -= this.OnProblemSolverAssign;
+                    problemSolver.Dispose();
                 }
-
-                OnDisposeProblemSolver(this.ProblemSolver);
-
-                this.Context?.Dispose();
             }
+
+            OnDisposeProblemSolver(this.ProblemSolver);
+
+            this.Context?.Dispose();
+
+            base.OnDisposing();
         }
     }
 }
