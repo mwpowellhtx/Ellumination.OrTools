6c238508b87f0261a3a2a760746307cc8771d844
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-21 15:17:24 -0500

    Approaching time window case study tests.

---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/Scopes/TimeWindowCaseStudyScope.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using DistanceMatrix = Distances.DistanceMatrix;
+    using static TestFixtureBase;
+
+    /// <inheritdoc/>
+    public class TimeWindowCaseStudyScope : CaseStudyScope<DistanceMatrix
+        , RoutingContext, DefaultRoutingAssignmentEventArgs, DefaultRoutingProblemSolver>
+    {
+        /// <summary>
+        /// Gets the VehicleCount, <c>4</c>.
+        /// </summary>
+        /// <value>4</value>
+        internal override int VehicleCount { get; } = 4;
+
+        /// <summary>
+        /// Gets the Vehicle Capacity for all vehicles, <c>30</c>.
+        /// </summary>
+        /// <value>30</value>
+        internal override long VehicleCap { get; } = 30;
+
+        /// <summary>
+        /// Gets the Maximum Slack, <c>30</c>.
+        /// </summary>
+        /// <value>30</value>
+        internal override long? SlackMaximum { get; } = 30;
+
+        /// <summary>
+        /// Gets whether to Zero Accumulator, <c>false</c>.
+        /// </summary>
+        /// <value>false</value>
+        internal override bool? ZeroAccumulator { get; } = default(bool);
+
+        // TODO: TBD: we could potentially refactor this sort of initialization to the test fixtures themselves...
+        // TODO: TBD: rather than do them here...
+        /// <summary>
+        /// Gets the ExpectedPaths.
+        /// </summary>
+        internal override IList<int[]> ExpectedPaths { get; set; } = Range(
+            Range(0, 8, 6, 2, 5, 0).ToArray()
+            , Range(0, 7, 1, 4, 3, 0).ToArray()
+            , Range(0, 9, 10, 16, 14, 0).ToArray()
+            , Range(0, 12, 11, 15, 13, 0).ToArray()).ToList();
+
+        private IEnumerable<int?> _expectedRouteDistances;
+
+        /// <summary>
+        /// Gets the ExpectedRouteDistances.
+        /// </summary>
+        internal virtual IEnumerable<int?> ExpectedRouteDistances => this._expectedRouteDistances ?? (
+            this._expectedRouteDistances = Range(0, this.VehicleCount).Select(_ => (int?)1552).ToArray()
+        );
+
+        // TODO: TBD: what is the proper distance unit?
+        /// <summary>
+        /// Gets the Vehicle Routing DistanceUnit, <c>&quot;m&quot;</c>
+        /// for lack of a better definition.
+        /// </summary>
+        /// <value>m</value>
+        protected override string DistanceUnit { get; } = "m";
+
+        /// <inheritdoc/>
+        protected override int?[,] MatrixValues { get; } = {
+            { 0, 6, 9, 8, 7, 3, 6, 2, 3, 2, 6, 6, 4, 4, 5, 9, 7 }
+            , { 6, 0, 8, 3, 2, 6, 8, 4, 8, 8, 13, 7, 5, 8, 12, 10, 14 }
+            , { 9, 8, 0, 11, 10, 6, 3, 9, 5, 8, 4, 15, 14, 13, 9, 18, 9 }
+            , { 8, 3, 11, 0, 1, 7, 10, 6, 10, 10, 14, 6, 7, 9, 14, 6, 16 }
+            , { 7, 2, 10, 1, 0, 6, 9, 4, 8, 9, 13, 4, 6, 8, 12, 8, 14 }
+            , { 3, 6, 6, 7, 6, 0, 2, 3, 2, 2, 7, 9, 7, 7, 6, 12, 8 }
+            , { 6, 8, 3, 10, 9, 2, 0, 6, 2, 5, 4, 12, 10, 10, 6, 15, 5 }
+            , { 2, 4, 9, 6, 4, 3, 6, 0, 4, 4, 8, 5, 4, 3, 7, 8, 10 }
+            , { 3, 8, 5, 10, 8, 2, 2, 4, 0, 3, 4, 9, 8, 7, 3, 13, 6 }
+            , { 2, 8, 8, 10, 9, 2, 5, 4, 3, 0, 4, 6, 5, 4, 3, 9, 5 }
+            , { 6, 13, 4, 14, 13, 7, 4, 8, 4, 4, 0, 10, 9, 8, 4, 13, 4 }
+            , { 6, 7, 15, 6, 4, 9, 12, 5, 9, 6, 10, 0, 1, 3, 7, 3, 10 }
+            , { 4, 5, 14, 7, 6, 7, 10, 4, 8, 5, 9, 1, 0, 2, 6, 4, 8 }
+            , { 4, 8, 13, 9, 8, 7, 10, 3, 7, 4, 8, 3, 2, 0, 4, 5, 6 }
+            , { 5, 12, 9, 14, 12, 6, 6, 7, 3, 3, 4, 7, 6, 4, 0, 9, 2 }
+            , { 9, 10, 18, 6, 8, 12, 15, 8, 13, 9, 13, 3, 4, 5, 9, 0, 9 }
+            , { 7, 14, 9, 16, 14, 8, 5, 10, 6, 5, 4, 10, 8, 6, 2, 9, 0 }
+        };
+
+        /// <summary>
+        /// Gets the TimeWindows for use with the Model. The First window is that of the Depot.
+        /// </summary>
+        internal (long start, long end)[] TimeWindows { get; } = {
+            (0, 5)
+            , (7, 12)
+            , (10, 15)
+            , (16, 18)
+            , (10, 13)
+            , (0, 5)
+            , (5, 10)
+            , (0, 4)
+            , (5, 10)
+            , (0, 3)
+            , (10, 16)
+            , (10, 15)
+            , (0, 5)
+            , (5, 10)
+            , (7, 8)
+            , (10, 15)
+            , (11, 15)
+        };
+
+        /// <inheritdoc/>
+        protected override DistanceMatrix CreateMatrix() => new DistanceMatrix(this.MatrixValues);
+
+        /// <inheritdoc/>
+        protected override RoutingContext CreateContext() => new RoutingContext(this.Matrix.Length, this.VehicleCount, this.Depot);
+
+        /// <inheritdoc/>
+        public TimeWindowCaseStudyScope(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Returns the Maximum of the <paramref name="val1"/> and <paramref name="val2"/>
+        /// values.
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        /// <remarks>Note that we included this here because a <see cref="Math.Max(int, int)"/>
+        /// reference was included in the web site example, but we do not find any evidence
+        /// of this in the subsequent report.</remarks>
+        /// <see cref="Math.Max(int, int)"/>
+        private static int Max(int val1, int val2) => Math.Max(val1, val2);
+    }
+}
---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/TimeWindowCaseStudyTests.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#if false
+
+    /* TODO: TBD: this one will be key, we will most likely need to
+     * include additional details in the solution/assignment event
+     * handler in order to facilitate the intermittent bits, extrapolating
+     * dimension, dim vars, mins, maxes, etc... I think we have the basics represented.
+     * what we may not have there yet are bits like the assignment, what to do with
+     * variables, mins, maxes, etc... */
+
+    /// <summary>
+    ///   Print the solution.
+    /// </summary>
+    static void PrintSolution(in DataModel data, in RoutingModel routing, in RoutingIndexManager manager,
+                              in Assignment solution)
+    {
+        RoutingDimension timeDimension = routing.GetMutableDimension("Time");
+        // Inspect solution.
+        long totalTime = 0;
+        for (int i = 0; i < data.VehicleNumber; ++i)
+        {
+            Console.WriteLine("Route for Vehicle {0}:", i);
+            var index = routing.Start(i);
+            while (routing.IsEnd(index) == false)
+            {
+                var timeVar = timeDimension.CumulVar(index);
+                Console.Write("{0} Time({1},{2}) -> ", manager.IndexToNode(index), solution.Min(timeVar),
+                              solution.Max(timeVar));
+                index = solution.Value(routing.NextVar(index));
+            }
+            var endTimeVar = timeDimension.CumulVar(index);
+            Console.WriteLine("{0} Time({1},{2})", manager.IndexToNode(index), solution.Min(endTimeVar),
+                              solution.Max(endTimeVar));
+            Console.WriteLine("Time of the route: {0}min", solution.Min(endTimeVar));
+            totalTime += solution.Min(endTimeVar);
+        }
+        Console.WriteLine("Total time of all routes: {0}min", totalTime);
+    }
+
+#endif // false
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
+{
+    using Google.Protobuf.WellKnownTypes;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+    // TODO: TBD: making an alias here because we think we may refactor to collections suite...
+    using DistanceMatrix = Distances.DistanceMatrix;
+    using RoutingModel = Google.OrTools.ConstraintSolver.RoutingModel;
+    using FirstSolutionStrategyType = Google.OrTools.ConstraintSolver.FirstSolutionStrategy.Types.Value;
+    using LocalSearchMetaheuristicType = Google.OrTools.ConstraintSolver.LocalSearchMetaheuristic.Types.Value;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <see cref="!:https://developers.google.com/optimization/routing/vrptw"/>
+    public class TimeWindowCaseStudyTests : OrToolsRoutingCaseStudyTests<DistanceMatrix, RoutingContext
+        , DefaultRoutingAssignmentEventArgs, DefaultRoutingProblemSolver, TimeWindowCaseStudyScope>
+    {
+        public TimeWindowCaseStudyTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected class TimeWindowDimension : Dimension
+        {
+            /// <summary>
+            /// Gets the Scope for Private use.
+            /// </summary>
+            private TimeWindowCaseStudyScope Scope { get; }
+
+            /// <summary>
+            /// Gets the Matrix for Private use.
+            /// </summary>
+            private DistanceMatrix Matrix => this.Scope.Matrix;
+
+            /// <summary>
+            /// Constructs the dimension.
+            /// </summary>
+            /// <param name="context"></param>
+            /// <param name="scope"></param>
+            /// <see cref="!:https://developers.google.com/optimization/routing/tsp#distance_callback"/>
+            public TimeWindowDimension(RoutingContext context, TimeWindowCaseStudyScope scope)
+                : base(context, scope.DimensionCoefficient)
+            {
+                if (this.Coefficient > 0)
+                {
+                    this.Scope = scope;
+
+                    var evaluatorIndex = this.RegisterTransitEvaluationCallback(this.OnEvaluateTransit);
+
+                    // TODO: TBD: with parameters that could be captured in a context, etc...
+                    this.AddDimension(evaluatorIndex, scope.VehicleCap
+                        , scope.SlackMaximum ?? default
+                        , scope.ZeroAccumulator ?? default
+                    );
+
+                    {
+                        // TODO: TBD: could/should probably refactor this to a separate method...
+                        // https://developers.google.com/optimization/routing/vrptw#time_window_constraints
+
+                        var this_Context_DepotCoords = this.Context.DepotCoordinates.ToArray();
+
+                        bool DepotCoordinatesDoNotContain(int _) => !this_Context_DepotCoords.Contains(_);
+
+                        // TODO: TBD: we may want to follow a similar tract here re: RC versus RoutingIndexManager...
+                        // TODO: TBD: however we just prefer to abstract those sorts of bits through the RC...
+                        // TODO: TBD: if there is a case for it, we will, but for now, the interface is sufficient...
+                        void OnSetCumulVarTimeWindow(int node, in (long start, long end) tw) => this.SetCumulVarRange(
+                            node, tw, (RoutingContext _, int i) => _.NodeToIndex(i)
+                        );
+
+                        void OnSetCumulVarRange(int node) => OnSetCumulVarTimeWindow(
+                            node
+                            , scope.TimeWindows.ElementAt(node)
+                        );
+
+                        // Add Time Window constraints for each Node except the Depot(s).
+                        var nodes = Enumerable.Range(default, this.Context.NodeCount)
+                            .Where(DepotCoordinatesDoNotContain).ToList();
+
+                        nodes.ForEach(OnSetCumulVarRange);
+                    }
+
+                    {
+                        // Add time window constraints for each vehicle start node.
+                        void OnSetVehicleTimeWindow(int vehicle) => this.SetCumulVarRange(
+                            vehicle, scope.TimeWindows.First(), (RoutingModel model, int i) => model.Start(i)
+                        );
+
+                        var vehicles = Enumerable.Range(default, scope.VehicleCount).ToList();
+
+                        vehicles.ForEach(OnSetVehicleTimeWindow);
+                    }
+
+                    {
+                        // Arrange for each Vehicle Start and End Finalizer concerns.
+                        void OnArrangeVehicleFinalizers(int vehicle)
+                        {
+                            // Illustrating how we can leverage either the root methods or extension methods.
+                            this.AddVariableMinimizedByFinalizer(vehicle, (RoutingContext _, int i) => _.Model.Start(i));
+                            this.AddVariableMinimizedByFinalizer(vehicle, (RoutingModel model, int i) => model.End(i));
+                        }
+
+                        var vehicles = Enumerable.Range(default, scope.VehicleCount).ToList();
+
+                        vehicles.ForEach(OnArrangeVehicleFinalizers);
+                    }
+
+                    // TODO: TBD: I'm not sure we actually ever add the dimension here ...
+                    // TODO: TBD: should review the example again to make sure that we actually should/are/do... (take your pick)
+                    this.SetArcCostEvaluators(evaluatorIndex);
+                }
+            }
+
+            /// <summary>
+            /// Returns the result after Evaluating <see cref="Matrix"/> given
+            /// <paramref name="fromNode"/> and <paramref name="toNode"/>.
+            /// </summary>
+            /// <param name="fromNode"></param>
+            /// <param name="toNode"></param>
+            /// <returns></returns>
+            private long OnEvaluateTransit(int fromNode, int toNode) => this.Matrix[fromNode, toNode] ?? default;
+
+            /// <summary>
+            /// Returns the result after Evaluating <see cref="Matrix"/> given
+            /// <paramref name="fromIndex"/> and <paramref name="toIndex"/>.
+            /// </summary>
+            /// <param name="fromIndex"></param>
+            /// <param name="toIndex"></param>
+            /// <returns></returns>
+            /// <see cref="!:https://developers.google.com/optimization/routing/tsp#distance_callback"/>
+            private long OnEvaluateTransit(long fromIndex, long toIndex) => this.OnEvaluateTransit(
+                this.Context.IndexToNode(fromIndex), this.Context.IndexToNode(toIndex)
+            );
+        }
+
+        ///// <summary>
+        ///// Arranges the Circuit Board Case Study <paramref name="scope"/>.
+        ///// </summary>
+        ///// <param name="scope"></param>
+        //protected virtual void OnArrangeTimeWindowCaseStudyScope(TimeWindowCaseStudyScope scope)
+        //{
+        //    scope.ExpectedTotalDistance = 2790;
+        //    scope.ExpectedPaths = Range<int[]>(
+        //            Range(0, 1, 279, 2, 278, 277, 247, 248, 249, 246, 244, 243, 242, 241, 240
+        //                , 239, 238, 237, 236, 235, 234, 233, 232, 231, 230, 245, 250, 229, 228
+        //                , 227, 226, 225, 224, 223, 222, 221, 220, 219, 218, 217, 216, 215, 214
+        //                , 213, 212, 211, 210, 209, 208, 251, 254, 255, 257, 256, 253, 252, 207
+        //                , 206, 205, 204, 203, 202, 142, 141, 146, 147, 140, 139, 265, 136, 137
+        //                , 138, 148, 149, 177, 176, 175, 178, 179, 180, 181, 182, 183, 184, 186
+        //                , 185, 192, 196, 197, 198, 144, 145, 143, 199, 201, 200, 195, 194, 193
+        //                , 191, 190, 189, 188, 187, 163, 164, 165, 166, 167, 168, 169, 171, 170
+        //                , 172, 105, 106, 104, 103, 107, 109, 110, 113, 114, 116, 117, 61, 62
+        //                , 63, 65, 64, 84, 85, 115, 112, 86, 83, 82, 87, 111, 108, 89, 90, 91
+        //                , 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 79, 88, 81, 80, 78
+        //                , 77, 76, 74, 75, 73, 72, 71, 70, 69, 66, 68, 67, 57, 56, 55, 54
+        //                , 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 58, 60, 59, 42, 41
+        //                , 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 124, 123, 122, 121
+        //                , 120, 119, 118, 156, 157, 158, 173, 162, 161, 160, 174, 159, 150, 151
+        //                , 155, 152, 154, 153, 128, 129, 130, 131, 18, 19, 20, 127, 126, 125, 28
+        //                , 27, 26, 25, 21, 24, 22, 23, 13, 12, 14, 11, 10, 9, 7, 8, 6, 5
+        //                , 275, 274, 273, 272, 271, 270, 15, 16, 17, 132, 133, 269, 268, 134
+        //                , 135, 267, 266, 264, 263, 262, 261, 260, 258, 259, 276, 3, 4, 0).ToArray()
+        //        ).ToList();
+        //}
+
+        /// <summary>
+        /// Do some TimeWindowBackground.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="matrix"></param>
+        /// <param name="expectedPaths"></param>
+        [Background]
+        public void TimeWindowBackground(TimeWindowCaseStudyScope scope, DistanceMatrix matrix, IEnumerable<int[]> expectedPaths)
+        {
+            $"Verify {nameof(scope)} not null".x(() =>
+                scope = this.Scope.AssertNotNull()
+            );
+
+            $"Verify {nameof(scope)}.{nameof(scope.Matrix)} not null".x(() =>
+                matrix = scope.Matrix.AssertNotNull()
+            );
+
+            $"Arrange {nameof(scope)}.{nameof(scope.ExpectedTotalDistance)}".x(() =>
+                expectedPaths = scope.ExpectedPaths.AssertEqual(scope.VehicleCount, x => x.Count)
+            );
+
+            // TODO: TBD: which even this is a bit boilerplate, for academic case study...
+            $"Add {nameof(TimeWindowDimension)} to {nameof(scope)}.{nameof(scope.Context)}".x(
+                () => scope.Context.AddDefaultDimension<TimeWindowDimension>(scope)
+            );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="!:https://developers.google.com/optimization/routing/tsp#main"/>
+        /// <param name="scope"></param>
+        [Scenario(Skip = "Remains an issue the expected solution is similar to a point then falls apart.")]
+        public void Verify_ProblemSolver_Solution(TimeWindowCaseStudyScope scope)
+        {
+            $"Verify {nameof(scope)}".x(() => scope = this.Scope.AssertNotNull());
+
+            void OnConfigureSearchParameters(object sender, RoutingSearchParametersEventArgs e)
+            {
+                var e_searchParams = e.SearchParameters;
+                e_searchParams.FirstSolutionStrategy = FirstSolutionStrategyType.PathCheapestArc;
+                //e_searchParams.LocalSearchMetaheuristic = LocalSearchMetaheuristicType.GuidedLocalSearch;
+                //e_searchParams.TimeLimit = 30L.AsDuration();
+                e_searchParams.LogSearch = true;
+            }
+
+            void OnConfigureSearchParametersCallback()
+            {
+                this.Scope.ProblemSolver.ConfigureSearchParameters += OnConfigureSearchParameters;
+            }
+
+            void OnRollbackSearchParametersCallback()
+            {
+                this.Scope.ProblemSolver.ConfigureSearchParameters -= OnConfigureSearchParameters;
+            }
+
+            $"Configure search parameters callbacks".x(OnConfigureSearchParametersCallback)
+                .Rollback(OnRollbackSearchParametersCallback);
+
+            // TODO: TBD: this is starting to look like really boilerplate stuff as well...
+            $"Solve routing problem given {nameof(scope)}.{nameof(scope.Context)}".x(
+                () => scope.ProblemSolver.Solve(scope.Context)
+            );
+        }
+
+        /// <summary>
+        /// Verifies the Circuit Board Case Study <paramref name="scope"/>.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <see cref="!:https://developers.google.com/optimization/routing/tsp#solution2"/>
+        /// <see cref="!:https://github.com/google/or-tools/discussions/2249">See discussion concerning various aspects</see>
+        protected virtual void OnVerifyTimeWindowSolutionScope(TimeWindowCaseStudyScope scope) =>
+            scope.AssertNotNull().VerifyAndReportSolution();
+
+        /// <summary>
+        /// Verifies that the <paramref name="scope"/> is correct according to what
+        /// we expected. We verify given the Optimization Routing TSP Solution.
+        /// </summary>
+        /// <param name="scope"></param>
+        protected override void OnVerifySolutionScope(TimeWindowCaseStudyScope scope)
+        {
+            base.OnVerifySolutionScope(scope);
+
+            this.OnVerifyTimeWindowSolutionScope(scope);
+        }
+    }
+}
