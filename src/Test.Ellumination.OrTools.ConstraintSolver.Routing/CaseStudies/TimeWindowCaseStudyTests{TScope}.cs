f12cfd15e9925b6a201542b58a7e1ba8d4008e4d
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-24 12:52:44 -0500

    Updated tests refactored and clarified bits concerning examples.

---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/TimeWindowCaseStudyTests{TScope}.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#if false
+
+    /* TODO: TBD: this one will be key, we will most likely need to
+     * include additional details in the solution/assignment event
+     * handler in order to facilitate the intermittent bits, extrapolating
+     * dimension, dim vars, mins, maxes, etc... I think we have the basics represented.
+     * what we may not have there yet are bits like the assignment, what to do with
+     * variables, mins, maxes, etc... */
+
+    /// <summary>
+    ///   Print the solution.
+    /// </summary>
+    static void PrintSolution(in DataModel data, in RoutingModel model, in RoutingIndexManager manager, in Assignment solution)
+    {
+        // TODO: TBD: we may need to expose dimensional lookups after all...
+        // TODO: TBD: which in this context, and that of the event pub/sub, makes a certain amount of sense...
+        RoutingDimension timeDimension = model.GetMutableDimension("Time");
+        // Inspect solution.
+        var totalTime = default(long);
+        for (var vehicle = 0; vehicle < data.VehicleNumber; vehicle++)
+        {
+            long index;
+            Console.WriteLine("Route for Vehicle {0}:", vehicle);
+            for (index = model.Start(vehicle); !model.IsEnd(index); index = solution.Value(model.NextVar(index)))
+            {
+                // TODO: TBD: would it be worthwhile including the cumulvars for each dimension at the given index?
+                // TODO: TBD: in any event, starting to sound like we need to define a first class descriptor...
+                // TODO: TBD: rather than the ad-hoc value tuple... since we need "both" indexes, etc...
+                var timeVar = timeDimension.CumulVar(index);
+                Console.Write("{0} Time({1},{2}) -> ", manager.IndexToNode(index), solution.Min(timeVar), solution.Max(timeVar));
+            }
+            var endTimeVar = timeDimension.CumulVar(index);
+            Console.WriteLine("{0} Time({1},{2})", manager.IndexToNode(index), solution.Min(endTimeVar), solution.Max(endTimeVar));
+            Console.WriteLine("Time of the route: {0}min", solution.Min(endTimeVar));
+            totalTime += solution.Min(endTimeVar);
+        }
+        Console.WriteLine("Total time of all routes: {0}min", totalTime);
+    }
+
+#endif // false
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+    // TODO: TBD: making an alias here because we think we may refactor to collections suite...
+    using Assignment = Google.OrTools.ConstraintSolver.Assignment;
+    using DistanceMatrix = Distances.DistanceMatrix;
+    using RoutingDimension = Google.OrTools.ConstraintSolver.RoutingDimension;
+    using FirstSolutionStrategyType = Google.OrTools.ConstraintSolver.FirstSolutionStrategy.Types.Value;
+    using static String;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TScope"></typeparam>
+    /// <typeparam name="TDimension"></typeparam>
+    /// <see cref="!:https://developers.google.com/optimization/routing/vrptw"/>
+    public abstract class TimeWindowCaseStudyTests<TScope, TDimension> : OrToolsRoutingCaseStudyTests<
+        DistanceMatrix, RoutingContext, DefaultRoutingAssignmentEventArgs, DefaultRoutingProblemSolver
+        , TScope>
+        where TScope : TimeWindowCaseStudyScope
+        where TDimension : TimeWindowDimension
+    {
+        protected TimeWindowCaseStudyTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scope"></param>
+        protected virtual void OnBackgroundAddDimensions(TScope scope)
+        {
+        }
+
+        /// <summary>
+        /// Do some TimeWindowBackground.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="context"></param>
+        /// <param name="matrix"></param>
+        /// <param name="expectedPaths"></param>
+        [Background]
+        public void TimeWindowBackground(TScope scope, RoutingContext context, DistanceMatrix matrix, IEnumerable<int[]> expectedPaths)
+        {
+            $"Verify {nameof(scope)} not null".x(() =>
+                scope = this.Scope.AssertNotNull()
+            );
+
+            $"Verify {nameof(scope)}.{nameof(scope.Matrix)} not null".x(() =>
+                matrix = scope.Matrix.AssertNotNull()
+            );
+
+            $"Verify {nameof(scope)}.{nameof(scope.Context)} not null".x(() =>
+                context = scope.Context.AssertNotNull()
+            ); ;
+
+            $"Arrange {nameof(scope)}.{nameof(scope.ExpectedTotalDistance)}".x(() =>
+                expectedPaths = scope.ExpectedPaths.AssertEqual(scope.VehicleCount, x => x.Count)
+            );
+
+            // TODO: TBD: which even this is a bit boilerplate, for academic case study...
+            $"Add {typeof(TDimension).Name} to {nameof(scope)}.{nameof(scope.Context)}".x(
+                () => scope.Context.AddDefaultDimension<TDimension>(scope)
+            );
+
+            IEnumerable<(int vehicle, int node, long index, long min, long max)> OnSummarizeAssignments(
+                RoutingDimension dim, Assignment solution, IEnumerable<RouteAssignmentItem> assignments) =>
+                assignments.Select(x => (vehicle: x.Vehicle, node: x.Node, index: x.Index, var: dim.CumulVar(x.Index)))
+                    .Select(_ => (_.vehicle, _.node, _.index, min: solution.Min(_.var), max: solution.Max(_.var)));
+
+            void OnScopeProblemSolverAfterAssignmentVehicle(object sender, DefaultRoutingAssignmentEventArgs e)
+            {
+                var context_Dim = context.Dimensions.OfType<TDimension>().AssertCollectionNotEmpty()
+                    .SingleOrDefault().AssertNotNull();
+
+                var e_Summary = OnSummarizeAssignments(context_Dim.MutableDimension, e.Solution, e.Assignments).ToArray();
+
+                var vehicle = e_Summary.Select(_ => _.vehicle).Distinct()
+                    .ToArray().AssertEqual(1, _ => _.Length).Single();
+
+                this.OutputHelper.WriteLine($"Route for vehicle {vehicle}");
+
+                this.OutputHelper.WriteLine(Join(" -> ", e_Summary.Select(_ => $"{_.node} Time({_.min}, {_.max})")));
+
+                // Verified by Distinct Vehicle above that we have at least One.
+                this.OutputHelper.WriteLine($"Time of the route: {e_Summary.Last().min} {scope.DistanceUnit}");
+            }
+
+            void OnScopeProblemSolverAfterAssignment(object sender, DefaultRoutingAssignmentEventArgs e)
+            {
+                var context_Dim = context.Dimensions.OfType<TDimension>().AssertCollectionNotEmpty()
+                    .SingleOrDefault().AssertNotNull();
+
+                var e_Summary = OnSummarizeAssignments(context_Dim.MutableDimension, e.Solution, e.Assignments).ToArray();
+
+                this.OutputHelper.WriteLine($"Total time of all routes: {e_Summary.GroupBy(_ => _.vehicle).Sum(_ => _.Last().min)}"
+                    + $" {scope.DistanceUnit}");
+            }
+
+            $"And connect appropriate event handlers".x(() =>
+            {
+                scope.ProblemSolver.AfterAssignmentVehicle += OnScopeProblemSolverAfterAssignmentVehicle;
+                scope.ProblemSolver.AfterAssignment += OnScopeProblemSolverAfterAssignment;
+            }).Rollback(() =>
+            {
+                // TODO: TBD: which is occurring "after" scope has been disposed?
+                // And disconnect on Rollback.
+                scope.ProblemSolver.AfterAssignmentVehicle -= OnScopeProblemSolverAfterAssignmentVehicle;
+                scope.ProblemSolver.AfterAssignment -= OnScopeProblemSolverAfterAssignment;
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="!:https://developers.google.com/optimization/routing/tsp#main"/>
+        /// <param name="scope"></param>
+        [Scenario/*(Skip = "Remains an issue the expected solution is similar to a point then falls apart.")*/]
+        public void Verify_ProblemSolver_Solution(TScope scope)
+        {
+            $"Verify {nameof(scope)}".x(() => scope = this.Scope.AssertNotNull());
+
+            void OnConfigureSearchParameters(object sender, RoutingSearchParametersEventArgs e)
+            {
+                var e_searchParams = e.SearchParameters;
+                e_searchParams.FirstSolutionStrategy = FirstSolutionStrategyType.PathCheapestArc;
+                e_searchParams.LogSearch = true;
+            }
+
+            void OnConfigureSearchParametersCallback()
+            {
+                this.Scope.ProblemSolver.ConfigureSearchParameters += OnConfigureSearchParameters;
+            }
+
+            void OnRollbackSearchParametersCallback()
+            {
+                this.Scope.ProblemSolver.ConfigureSearchParameters -= OnConfigureSearchParameters;
+            }
+
+            $"Configure search parameters callbacks".x(OnConfigureSearchParametersCallback)
+                .Rollback(OnRollbackSearchParametersCallback);
+
+            // TODO: TBD: this is starting to look like really boilerplate stuff as well...
+            $"Solve routing problem given {nameof(scope)}.{nameof(scope.Context)}".x(
+                () => scope.ProblemSolver.Solve(scope.Context)
+            );
+        }
+
+        /// <summary>
+        /// Verifies the Circuit Board Case Study <paramref name="scope"/>.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <see cref="!:https://developers.google.com/optimization/routing/tsp#solution2"/>
+        /// <see cref="!:https://github.com/google/or-tools/discussions/2249">See discussion concerning various aspects</see>
+        protected virtual void OnVerifyTimeWindowSolutionScope(TScope scope) =>
+            scope.AssertNotNull().VerifyAndReportSolution();
+
+        /// <summary>
+        /// Verifies that the <paramref name="scope"/> is correct according to what
+        /// we expected. We verify given the Optimization Routing TSP Solution.
+        /// </summary>
+        /// <param name="scope"></param>
+        protected override void OnVerifySolutionScope(TScope scope)
+        {
+            base.OnVerifySolutionScope(scope);
+
+            this.OnVerifyTimeWindowSolutionScope(scope);
+        }
+    }
+}
---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/TimeWindowResourceConstraintCaseStudyTests.cs
@@ -0,0 +1,75 @@
+﻿namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
+{
+    using Xunit.Abstractions;
+
+#if false
+    // https://developers.google.com/optimization/routing/cvrptw_resources#loading_constraints
+    /* The following code adds time windows for loading and unloading the vehicles at the depot.
+     * These windows, created by the method <c>FixedDurationIntervalVar</c>, are <em>variable
+     * time windows</em>, meaning they do not have fixed start and end times, unlike the time
+     * windows at the locations. The widths of the windows are specified by <c>vehicle_load_time</c>
+     * and <c>vehicle_unload_time</c>, which happen to be the same in this example. */
+    var solver = routing.solver();
+    var intervals = new IntervalVar[data.VehicleNumber * 2];
+    for (var i = 0; i < data.VehicleNumber; i++)
+    {
+        // Add load duration at start of routes.
+        intervals[2 * i] = solver.MakeFixedDurationIntervalVar(timeDimension.CumulVar(routing.Start(i)), data.VehicleLoadTime, "depot_interval");
+        //                                                                            ^^^^^^^^^^^^^      ^^^^^^^^^^^^^^^^^^^^ (?)
+        // Add unload duration at end of routes.
+        intervals[2 * i + 1] = solver.MakeFixedDurationIntervalVar(timeDimension.CumulVar(routing.End(i)), data.VehicleUnloadTime, "depot_interval");
+        //                                                                                ^^^^^^^^^^^      ^^^^^^^^^^^^^^^^^^^^^^ (?)
+    }
+#endif // false
+
+#if false
+    // https://developers.google.com/optimization/routing/cvrptw_resources#add-resource-constraints-at-the-depot
+    /* The following code creates the contraint that at most two vehicles can be loaded
+     * or unloaded at the same time.
+     * 
+     * <c>depot_capacity</c> is the maximum number of vehicles that can be loaded or unloaded
+     * at the same time, which is 2 in this example.
+     * 
+     * <c>depot_usage</c> is a vector containing the relative amounts of space required by each
+     * vehicle during loading, or unloading, respectively. In this example, we assume that all
+     * vehicles require the same amount of space, so <c>depot_usage</c> contains all ones. This
+     * means that the maximum number of vehicles that can be loaded at the same time is 2. */
+    var depot_usage = Enumerable.Repeat<long>(1, intervals.Length).ToArray();
+    solver.Add(solver.MakeCumulative(intervals, depot_usage, data.DepotCapacity, "depot"));
+#endif // false
+
+#if false
+    /*
+     *  Route for vehicle 0:
+     *   0 Time(5, 5) -> 8 Time(8, 8) -> 14 Time(11, 11) -> 16 Time(13, 13) -> 0 Time(20, 20)
+     *  Time of the route: 20 min
+     *
+     *  Route for vehicle 1:
+     *   0 Time(0, 0) -> 12 Time(4, 4) -> 13 Time(6, 6) -> 15 Time(11, 11) -> 11 Time(14, 14) -> 0 Time(20, 20)
+     *  Time of the route: 20 min
+     *
+     *  Route for vehicle 2:
+     *   0 Time(5, 5) -> 7 Time(7, 7) -> 1 Time(11, 11) -> 4 Time(13, 13) -> 3 Time(14, 14) -> 0 Time(25, 25)
+     *  Time of the route: 25 min
+     *
+     *  Route for vehicle 3:
+     *   0 Time(0, 0) -> 9 Time(2, 3) -> 5 Time(4, 5) -> 6 Time(6, 9) -> 2 Time(10, 12) -> 10 Time(14, 16) -> 0 Time(25, 25)
+     *  Time of the route: 25 min
+     *
+     *  Total time of all routes: 90 min
+     */
+#endif // false
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <see cref="!:https://developers.google.com/optimization/routing/vrptw"/>
+    public class TimeWindowResourceConstraintCaseStudyTests : TimeWindowCaseStudyTests<
+        TimeWindowResourceConstraintCaseStudyScope, TimeWindowResourceConstraintDimension>
+    {
+        public TimeWindowResourceConstraintCaseStudyTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+    }
+}
