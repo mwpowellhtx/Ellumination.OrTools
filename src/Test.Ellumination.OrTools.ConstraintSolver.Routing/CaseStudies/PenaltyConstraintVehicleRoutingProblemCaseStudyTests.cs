0c25c8b8bd19728562bd9609d9b5fe982c238571
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-25 19:56:53 -0500

    Added Penalty scope and tests.

---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/PenaltyConstraintVehicleRoutingProblemCaseStudyTests.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
+{
+    using Google.Protobuf.WellKnownTypes;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xwellbehaved;
+    using DistanceMatrix = Distances.DistanceMatrix;
+    using FirstSolutionStrategyType = Google.OrTools.ConstraintSolver.FirstSolutionStrategy.Types.Value;
+    using LocalSearchMetaheuristicType = Google.OrTools.ConstraintSolver.LocalSearchMetaheuristic.Types.Value;
+    using static String;
+
+    /// <summary>
+    /// ... <see cref="!:https://developers.google.com/optimization/routing/penalties">web
+    /// site example</see>.
+    /// </summary>
+    /// <see cref="!:https://developers.google.com/optimization/routing/penalties"/>
+    public class PenaltyConstraintVehicleRoutingCaseStudyTests : VehicleRoutingCaseStudyTests<PenaltyConstraintVehicleRoutingCaseStudyScope>
+    {
+        public PenaltyConstraintVehicleRoutingCaseStudyTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Establishes a NodeDemandDimension for use during the routing problem solver.
+        /// </summary>
+        private class DisjunctionDimension : Dimension
+        {
+            /// <summary>
+            /// Gets the Scope for Private use.
+            /// </summary>
+            private PenaltyConstraintVehicleRoutingCaseStudyScope Scope { get; }
+
+            /// <summary>
+            /// Constructs a Node Demand dimension.
+            /// </summary>
+            /// <param name="context"></param>
+            /// <param name="scope"></param>
+            /// <see cref="!:https://developers.google.com/optimization/routing/penalties#add-the-capacity-constraints-and-penalties"/>
+            public DisjunctionDimension(RoutingContext context, PenaltyConstraintVehicleRoutingCaseStudyScope scope)
+                : base(context, scope.DimensionCoefficient)
+            {
+                var scope_Penalty = scope.Penalty ?? default;
+
+                if (this.Coefficient > 0 && scope_Penalty > 0)
+                {
+                    this.Scope = scope;
+
+                    // Which we think is more accurate than assuming the Zero Node.
+                    var context_DepotCoords = this.Context.DepotCoordinates.ToArray();
+
+                    var nodes = Enumerable.Range(0, this.Context.NodeCount).Where(_ => !context_DepotCoords.Contains(_)).ToList();
+
+                    void OnAddDisjunction(int node) => this.AddDisjunction(scope_Penalty, node);
+
+                    nodes.ForEach(OnAddDisjunction);
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override PenaltyConstraintVehicleRoutingCaseStudyScope OnVerifyInitialScope(PenaltyConstraintVehicleRoutingCaseStudyScope scope)
+        {
+            scope = base.OnVerifyInitialScope(scope);
+
+            scope.ActualTotalDistance.AssertEqual(default);
+
+            scope.Matrix.AssertEqual(scope.Context.NodeCount, x => x.Width);
+            scope.Matrix.AssertEqual(scope.Context.NodeCount, x => x.Height);
+
+            scope.Context.AssertEqual(scope.VehicleCount, x => x.VehicleCount);
+
+            return scope;
+        }
+
+        /// <summary>
+        /// Performs the Background for the CVRP problem.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <see cref="!:https://developers.google.com/optimization/routing/penalties"/>
+        [Background]
+        public void PenaltyConstraintVehicleRoutingBackground(PenaltyConstraintVehicleRoutingCaseStudyScope scope)
+        {
+            $"Verify {nameof(scope)}".x(() => scope = this.Scope.AssertNotNull());
+
+            $"Add {nameof(DisjunctionDimension)} dimension to {nameof(scope)}.{nameof(scope.Context)}".x(
+                () => scope.Context.AddDefaultDimension<DisjunctionDimension>(scope)
+            );
+        }
+
+        /// <summary>
+        /// Demonstrates a scenario in which the Solution Agrees with the Optimization Routing
+        /// Traveling Salesman Problem illustration.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <see cref="!:https://developers.google.com/optimization/routing/cvrp"/>
+        /// <see cref="!:https://developers.google.com/optimization/routing/cvrp#entire_program"/>
+        [Scenario]
+        public void Verify_ProblemSolver_Constraint_Solution(PenaltyConstraintVehicleRoutingCaseStudyScope scope)
+        {
+            $"Verify {nameof(scope)}".x(() => scope = this.Scope.AssertNotNull());
+
+            void OnScopeProblemSolverConfigureSearch(object sender, RoutingSearchParametersEventArgs e)
+            {
+                // Setting first solution heuristic.
+                e.SearchParameters.FirstSolutionStrategy = FirstSolutionStrategyType.PathCheapestArc;
+                e.SearchParameters.LocalSearchMetaheuristic = LocalSearchMetaheuristicType.GuidedLocalSearch;
+                e.SearchParameters.TimeLimit = 1L.AsDuration();
+                //e.SearchParameters.TimeLimit = 10L.AsDuration();
+            }
+
+            void OnArrangeProblemSolverConfigSearch()
+            {
+                scope.ProblemSolver.ConfigureSearchParameters += OnScopeProblemSolverConfigureSearch;
+            }
+
+            void OnRollbackProblemSolverConfigSearch()
+            {
+                scope.ProblemSolver.ConfigureSearchParameters -= OnScopeProblemSolverConfigureSearch;
+            }
+
+            $"Configure {nameof(scope)}.{nameof(scope.ProblemSolver)}.{nameof(scope.ProblemSolver.ConfigureSearchParameters)}"
+                .x(OnArrangeProblemSolverConfigSearch)
+                .Rollback(OnRollbackProblemSolverConfigSearch)
+                ;
+
+            $"Solve routing problem given {nameof(scope)}.{nameof(scope.Context)}".x(
+                () => scope.ProblemSolver.Solve(scope.Context)
+            );
+        }
+
+        /// <summary>
+        /// Verifies that the <paramref name="scope"/> is correct according to what
+        /// we expected. We verify given the Optimization Routing TSP Solution.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <see cref="!:https://developers.google.com/optimization/routing/penalties#running-the-program"/>
+        protected override void OnVerifySolutionScope(PenaltyConstraintVehicleRoutingCaseStudyScope scope)
+        {
+            // TODO: TBD: factor this one better...
+            void Report()
+            {
+                var dropped = Range(0, scope.Context.NodeCount).Except(scope.SolutionPaths.SelectMany(_ => _).Distinct());
+
+                this.OutputHelper.WriteLine($"Dropped nodes: {Render(dropped.ToArray())}");
+
+                var scope_Context_DepotCoords = scope.Context.DepotCoordinates.ToArray();
+
+                var loads = Repeat<long>(default, scope.Context.VehicleCount).ToArray();
+
+                void OnReportEach((int vehicle, int[] path) tuple)
+                {
+                    this.OutputHelper.WriteLine($"{nameof(tuple.path)} for {nameof(tuple.vehicle)} {tuple.vehicle}:");
+
+                    // Enumerate the path in terms of "X Load(Y) -> ..."
+
+                    string RenderLoad(int node)
+                    {
+                        long load = scope_Context_DepotCoords.Contains(node)
+                            ? default
+                            : scope.Demands.ElementAt(node);
+                        return $"{node} {nameof(load)}({loads[tuple.vehicle] += load})";
+                    }
+
+                    this.OutputHelper.WriteLine(Join(" -> ", tuple.path.Select(RenderLoad)));
+
+                    int distance = tuple.path.Count() <= 2 ? default : tuple.path.Take(tuple.path.Length - 1)
+                        .Zip(tuple.path.Skip(1), (x, y) => scope.Matrix[x, y] ?? default).Sum();
+
+                    this.OutputHelper.WriteLine($"Distance of the {nameof(tuple.path)}: {distance}");
+                    this.OutputHelper.WriteLine($"Load of the {nameof(tuple.path)}: {loads[tuple.vehicle]}");
+                }
+
+                scope.SolutionPaths.Select((_, i) => (i, _.ToArray())).ToList().ForEach(OnReportEach);
+            }
+
+            Report();
+
+            base.OnVerifySolutionScope(scope);
+
+            scope.SolutionPaths.AssertEqual(scope.VehicleCount, x => x.Count);
+
+            void OnVerifyExpectedPath((int vehicle, int[] expectedPath) tuple) =>
+                scope.SolutionPaths[tuple.vehicle].AssertCollectionEqual(tuple.expectedPath);
+
+            scope.ExpectedPaths.AssertEqual(scope.VehicleCount, x => x.Count)
+                .Select((x, i) => (i, x)).ToList().ForEach(OnVerifyExpectedPath);
+        }
+    }
+}
---
+++ src/Test.Ellumination.OrTools.ConstraintSolver.Routing/CaseStudies/Scopes/PenaltyConstraintVehicleRoutingCaseStudyScope.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+#if false
+
+Objective: 7936
+
+Dropped nodes: 6 15
+
+Route for vehicle 0:
+ 0 Load(0) ->  9 Load(1) ->  14 Load(7) ->  16 Load(15) ->  0 Load(15)
+Distance of the route: 1324m
+Load of the route: 15
+
+Route for vehicle 1:
+ 0 Load(0) ->  12 Load(2) ->  11 Load(3) ->  4 Load(9) ->  3 Load(12) ->  1 Load(13) ->  0 Load(13)
+Distance of the route: 1872m
+Load of the route: 13
+
+Route for vehicle 2:
+ 0 Load(0) ->  7 Load(8) ->  13 Load(14) ->  0 Load(14)
+Distance of the route: 868m
+Load of the route: 14
+
+Route for vehicle 3:
+ 0 Load(0) ->  8 Load(8) ->  10 Load(10) ->  2 Load(11) ->  5 Load(14) ->  0 Load(14)
+Distance of the route: 1872m
+Load of the route: 14
+
+Total Distance of all routes: 5936m
+
+Total Load of all routes: 56
+
+#endif // false
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing.CaseStudies
+{
+    using Xunit;
+    using Xunit.Abstractions;
+    using static TestFixtureBase;
+
+    /// <inheritdoc/>
+    public class PenaltyConstraintVehicleRoutingCaseStudyScope : ConstraintVehicleRoutingCaseStudyScope
+    {
+        /// <summary>
+        /// Gets the Penalty associated with the Scope, for use with Disjunctions.
+        /// Defaults to <c>1000</c>.
+        /// </summary>
+        /// <value>1000</value>
+        /// <see cref="!:https://developers.google.com/optimization/routing/penalties#add-the-capacity-constraints-and-penalties"/>
+        internal override long? Penalty { get; } = 1000;
+
+        /// <summary>
+        /// Gets the Demands informing the Case Study tests.
+        /// </summary>
+        /// <see cref="!:https://developers.google.com/optimization/routing/penalties#create-the-data"/>
+        public override IEnumerable<long> Demands { get; } = Range<long>(
+            0, 1, 1, 3, 6, 3, 6, 8, 8, 1, 2, 1, 2, 6, 6, 8, 8).ToArray();
+
+        /// <inheritdoc/>
+        internal override IList<int[]> ExpectedPaths { get; set; } = Range(
+            Range(0, 9, 14, 16, 0).ToArray()
+            , Range(0, 12, 11, 4, 3, 1, 0).ToArray()
+            , Range(0, 7, 13, 0).ToArray()
+            , Range(0, 8, 10, 2, 5, 0).ToArray()).ToList();
+
+        /// <summary>
+        /// Gets the ExpectedRouteDistances.
+        /// </summary>
+        /// <see cref="!:https://developers.google.com/optimization/routing/penalties#running-the-program"/>
+        internal override IEnumerable<int?> ExpectedRouteDistances { get; } = Range<int?>(1324, 1872, 868, 5936);
+
+        /// <summary>
+        /// Constructs a Case Study Scope instance.
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        public PenaltyConstraintVehicleRoutingCaseStudyScope(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        // TODO: TBD: what is the proper distance unit?
+        /// <summary>
+        /// Gets the Vehicle Routing DistanceUnit, <c>&quot;m&quot;</c>
+        /// for lack of a better definition.
+        /// </summary>
+        /// <value>m</value>
+        internal override string DistanceUnit { get; } = "m";
+    }
+}
