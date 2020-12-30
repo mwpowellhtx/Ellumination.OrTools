647037eeed6c37008e8ee120ff49f8320892720a
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-29 21:00:40 -0500

    Added wrapper level parameter proxies.

---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/ModelParameters.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    using ConstraintSolverParameters = Google.OrTools.ConstraintSolver.ConstraintSolverParameters;
+    using RoutingModelParameters = Google.OrTools.ConstraintSolver.RoutingModelParameters;
+
+    /// <summary>
+    /// Offers an opportunity to adapt Model parameters into the wrapper framework.
+    /// </summary>
+    /// <see cref="!:https://github.com/google/or-tools/issues/2301">[dotnet::routing]
+    /// Unseal the sealed parameters</see>
+    /// <see cref="!:https://github.com/protocolbuffers/protobuf/issues/8169">[csharp]
+    /// Allow for unsealed message output to be generated</see>
+    public class ModelParameters : Parameters
+    {
+        /// <summary>
+        /// Advanced settings. If set to <c>true</c> reduction of the underlying constraint
+        /// model will be attempted when all vehicles have exactly the same cost structure.
+        /// This can result in significant speedups.
+        /// </summary>
+        public virtual bool ReduceVehicleCostModel { get; set; }
+
+        /// <summary>
+        /// Cache callback calls if the number of nodes in the model is less or equal to this
+        /// value.
+        /// </summary>
+        public virtual int MaxCallbackCacheSize { get; set; }
+
+        /// <summary>
+        /// Gets the Parameters to use in the underlying constraint solver.
+        /// </summary>
+        public virtual ConstraintSolverParameters SolverParameters { get; private set; } = new ConstraintSolverParameters();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal ModelParameters()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        internal ModelParameters(RoutingModelParameters source)
+            : base(source) =>
+            CopyFrom(this, source);
+
+        /// <summary>
+        /// Copies <paramref name="target"/> From the <paramref name="source"/>.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static ModelParameters CopyFrom(ModelParameters target, RoutingModelParameters source)
+        {
+            target.MaxCallbackCacheSize = source.MaxCallbackCacheSize;
+            target.ReduceVehicleCostModel = source.ReduceVehicleCostModel;
+            target.SolverParameters = source.SolverParameters;
+            return target;
+        }
+
+        /// <summary>
+        /// Copies <paramref name="source"/> To the <paramref name="target"/>.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static RoutingModelParameters CopyTo(ModelParameters source, RoutingModelParameters target)
+        {
+            target.MaxCallbackCacheSize = source.MaxCallbackCacheSize;
+            target.ReduceVehicleCostModel = source.ReduceVehicleCostModel;
+            target.SolverParameters = source.SolverParameters;
+            return target;
+        }
+
+        /// <summary>
+        /// Implicitly converts <paramref name="source"/> to a
+        /// <see cref="RoutingModelParameters"/> instance.
+        /// </summary>
+        /// <param name="source"></param>
+        public static implicit operator RoutingModelParameters(ModelParameters source) =>
+            source == null ? null : CopyTo(source, new RoutingModelParameters());
+
+        /// <summary>
+        /// Implicitly converts <paramref name="source"/> to a <see cref="ModelParameters"/>
+        /// instance.
+        /// </summary>
+        /// <param name="source"></param>
+        public static implicit operator ModelParameters(RoutingModelParameters source) =>
+            source == null ? null : CopyFrom(new ModelParameters(), source);
+    }
+}
---
+++ src/Ellumination.OrTools.ConstraintSolver.Routing/Parameters.cs
@@ -0,0 +1,23 @@
+﻿namespace Ellumination.OrTools.ConstraintSolver.Routing
+{
+    /// <summary>
+    /// Represents Parameters that can be used for configuring either the Model or its Search.
+    /// </summary>
+    public abstract class Parameters
+    {
+        /// <summary>
+        /// Constructs an instance of the <see cref="Parameters"/>.
+        /// </summary>
+        protected Parameters()
+        {
+        }
+
+        /// <summary>
+        /// Constructs an instance of the <see cref="Parameters"/>.
+        /// </summary>
+        /// <param name="_"></param>
+        protected Parameters(object _)
+        {
+        }
+    }
+}
