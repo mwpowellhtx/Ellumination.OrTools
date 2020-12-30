3fbcdc5826dd2a3ef2ad89959fe0a995ea3c528c
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-29 19:48:04 -0500

    Added core level ValueBridge base class. Also positioned the optional Boolean concept. However we will postpone expecting a Google dependency in the core project for the time being.

---
+++ src/Ellumination.OrTools.Core/Enumerations/OptionalBoolean.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//// TODO: TBD: given our "Core" assembly does not make a reference to Google.OrTools intentionally, this may not be the best factoring...
+//// TODO: TBD: we may reconsider whether such a dependency is warranted after all...
+//namespace Ellumination.OrTools.Enumerations
+//{
+//    using OptionalBooleanType = Google.OrTools
+//    /// <summary>
+//    /// A &quot;three-way&quot; or <em>tri-state</em> boolean: <c>unspecified</c>, <c>false</c>
+//    /// or <c>true</c>. We do not use the value of <c>1</c> to increase the chance to catch bugs.
+//    /// <br/>
+//    /// <br/>
+//    /// In <em>python</em>, a user may set a proto field of this type enum to a <c>boolean</c>
+//    /// value without type checks, if they set it to <c>True</c>, the proto validity code will
+//    /// catch it, because it will be cast to <c>1</c>, which is an invalid enum value. Note that
+//    /// if the user sets if to <c>False</c>, i.e. <c>0</c>, it will be caught by the routing
+//    /// library parameter validity check as well.
+//    /// </summary>
+//    /// <see cref="!:https://python.org"/>
+//    public enum OptionalBoolean
+//    {
+//        /// <summary>
+//        /// <c>0</c>
+//        /// </summary>
+//        BoolUnspecified = 0,
+
+//        /// <summary>
+//        /// <c>2</c>
+//        /// </summary>
+//        BoolFalse = 2,
+
+//        /// <summary>
+//        /// <c>3</c>
+//        /// </summary>
+//        BoolTrue = 3
+//    }
+//}
---
+++ src/Ellumination.OrTools.Core/ValueBridge{TAlpha, TBravo, TBridge}.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ellumination.OrTools
+{
+    // TODO: TBD: short of adopting a "lite" dependency on something like an Automapper...
+    /// <summary>
+    /// Override in order to declare a type specific Bridge between two Values of Type
+    /// <typeparamref name="TAlpha"/> and <typeparamref name="TBravo"/>, respectively.
+    /// </summary>
+    /// <typeparam name="TAlpha"></typeparam>
+    /// <typeparam name="TBravo"></typeparam>
+    /// <typeparam name="TBridge"></typeparam>
+    public abstract class ValueBridge<TAlpha, TBravo, TBridge>
+        where TBridge : ValueBridge<TAlpha, TBravo, TBridge>
+    {
+        /// <summary>
+        /// Gets the <typeparamref name="TAlpha"/> <see cref="Type"/>.
+        /// </summary>
+        protected static Type AlphaType { get; } = typeof(TAlpha);
+
+        /// <summary>
+        /// Gets the <typeparamref name="TBravo"/> <see cref="Type"/>.
+        /// </summary>
+        protected static Type BravoType { get; } = typeof(TBravo);
+
+        private TAlpha _alpha;
+        private TBravo _bravo;
+
+        /// <summary>
+        /// Override in order to specify the <typeparamref name="TAlpha"/>
+        /// and <typeparamref name="TBravo"/> Pairs.
+        /// </summary>
+        protected abstract IEnumerable<(TAlpha alpha, TBravo bravo)> Pairs { get; }
+
+        /// <summary>
+        /// Returns the Item associated with <paramref name="getter"/> after
+        /// <paramref name="selector"/> identifies the Pair.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="selector"></param>
+        /// <param name="getter"></param>
+        /// <returns></returns>
+        protected virtual T GetPairItem<T>(Func<(TAlpha alpha, TBravo bravo), bool> selector, Func<(TAlpha alpha, TBravo bravo), T> getter) =>
+            getter.Invoke(this.Pairs.Single(selector));
+
+        /// <summary>
+        /// Override in order to Convert <paramref name="value"/> to <typeparamref name="TAlpha"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected abstract TAlpha ConvertValue(TBravo value);
+
+        /// <summary>
+        /// Override in order to Convert <paramref name="value"/> to <typeparamref name="TBravo"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected abstract TBravo ConvertValue(TAlpha value);
+
+        /// <summary>
+        /// Gets or Sets the <typeparamref name="TAlpha"/> Value.
+        /// </summary>
+        public virtual TAlpha Alpha
+        {
+            get => this._alpha;
+            set => this._bravo = this.ConvertValue(this._alpha = value);
+        }
+
+        /// <summary>
+        /// Gets or Sets the <typeparamref name="TBravo"/> Value.
+        /// </summary>
+        public virtual TBravo Bravo
+        {
+            get => this._bravo;
+            set => this._alpha = this.ConvertValue(this._bravo = value);
+        }
+
+        /// <summary>
+        /// Constructs a new instance of the ValueBridge.
+        /// </summary>
+        /// <param name="value"></param>
+        internal ValueBridge(TAlpha value) => this.Alpha = value;
+
+        /// <summary>
+        /// Constructs a new instance of the ValueBridge.
+        /// </summary>
+        /// <param name="value"></param>
+        internal ValueBridge(TBravo value) => this.Bravo = value;
+
+        /// <summary>
+        /// Implicitly converts <paramref name="bridge"/> to <typeparamref name="TAlpha"/>.
+        /// </summary>
+        /// <param name="bridge"></param>
+        public static implicit operator TAlpha(ValueBridge<TAlpha, TBravo, TBridge> bridge) => bridge.Alpha;
+
+        /// <summary>
+        /// Implicitly converts <paramref name="bridge"/> to <typeparamref name="TBravo"/>.
+        /// </summary>
+        /// <param name="bridge"></param>
+        public static implicit operator TBravo(ValueBridge<TAlpha, TBravo, TBridge> bridge) => bridge.Bravo;
+
+        /// <summary>
+        /// Gets the <typeparamref name="TBridge"/> <see cref="Type"/>.
+        /// </summary>
+        protected static Type BridgeType { get; } = typeof(TBridge);
+
+        /// <summary>
+        /// Returns a Created <typeparamref name="TBridge"/> given the <paramref name="value"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns>Created <typeparamref name="TBridge"/> given the <paramref name="value"/>.</returns>
+        protected static TBridge CreateBridge<T>(T value) => (TBridge)Activator.CreateInstance(BridgeType, value);
+    }
+}
